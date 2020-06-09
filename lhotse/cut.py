42faf694e637709f7945d83db51f912fc0b0694c
Piotr Żelasko <petezor@gmail.com>
2020-06-09 14:15:15 -0400

    Documentation for CutSet; fix truncate bugs

--- lhotse/cut.py
+++ lhotse/cut.py
@@ -11,12 +11,13 @@ from lhotse.utils import Seconds, Decibels, overlaps, TimeSpan, overspans, Pathl
 
 
 # One of the design principles for Cuts is a maximally "lazy" implementation, e.g. when overlaying/mixing Cuts,
-# we'd rather sum the feature matrices only after somebody actually calls "load_features".
+# we'd rather sum the feature matrices only after somebody actually calls "load_features". It helps to avoid
+# an excessive storage size for data augmented in various ways.
 
 @dataclass
 class Cut:
     """
-    A Cut is a single segment that we'll train on. It contains the features corresponding to
+    A Cut is a single "segment" that we'll train on. It contains the features corresponding to
     a piece of a recording, with zero or more SupervisionSegments.
     """
     id: str
@@ -35,10 +36,16 @@ class Cut:
     # cut duration. They also might overlap.
     supervisions: List[SupervisionSegment]
 
+    @property
     def end(self) -> Seconds:
         return self.start + self.duration
 
     def load_features(self, root_dir: Optional[Pathlike] = None) -> np.ndarray:
+        """
+        Load the features from the underlying storage and cut them to the relevant
+        [begin, duration] region of the current Cut.
+        Optionally specify a `root_dir` prefix to prefix the features path with.
+        """
         return self.features.load(root_dir=root_dir, start=self.start, duration=self.duration)
 
     def truncate(
@@ -48,10 +55,25 @@ class Cut:
             until: Optional[Seconds] = None,
             keep_excessive_supervisions: bool = True
     ) -> 'Cut':
+        """
+        Returns a new Cut that is a sub-region of the current Cut. The `offset` parameter controls the start of the
+        new cut relative to the current Cut's start, and `until` parameter controls the new cuts end.
+        Since trimming may happen inside a SupervisionSegment, the caller has an option to either keep or discard
+        such supervisions with `keep_excessive_supervision` flag.
+        Note that no operation is done on the actual features - it's only during load_features() when the actual
+        changes happen (a subset of features is loaded).
+
+        Example:
+        >>> from math import isclose
+        >>> cut = Cut(id='x', channel=0, start=3.0, duration=8.0, features='dummy', supervisions=[])
+        >>> trimmed_cut = cut.truncate(offset=5.0, until=7.0)
+        >>> trimmed_cut.start == 5.0 and isclose(trimmed_cut.duration, 2.0) and isclose(cut.end, 7.0)
+        """
         new_start = self.start + offset
-        new_duration = self.duration if until is None else until
+        new_duration = self.duration - new_start if until is None else until - offset
         assert new_duration > 0.0
-        new_time_span = TimeSpan(start=new_start, end=new_duration)
+        assert new_start + new_duration <= self.start + self.duration + 1e-5
+        new_time_span = TimeSpan(start=new_start, end=new_start + new_duration)
         criterion = overlaps if keep_excessive_supervisions else overspans
         return Cut(
             id=str(uuid4()),
@@ -65,6 +87,13 @@ class Cut:
         )
 
     def overlay(self, other: 'Cut', offset_other_by: Seconds = 0.0, snr: Decibels = 0.0) -> 'MixedCut':
+        """
+        Overlay, or mix, this Cut with the `other` Cut. Optionally the `other` Cut may be shifted by `offset_other_by`
+        Seconds and scaled down (positive SNR) or scaled up (negative SNR).
+        Returns a MixedCut, which only keeps the information about the mix; actual mixing is performed
+        during the call to `load_features`.
+        """
+        # TODO: allow mixing more than one cut together (make MixedCut a "list" of cuts)
         return MixedCut(
             id=str(uuid4()),
             left_cut_id=self.id,
@@ -74,6 +103,13 @@ class Cut:
         )
 
     def append(self, other: 'Cut', snr: float) -> 'MixedCut':
+        """
+        Append the `other` Cut after the current Cut. Conceptually the same as `overlay` but with an offset
+        matching the current cuts length. Optionally scale down (positive SNR) or scale up (negative SNR)
+        the `other` cut.
+        Returns a MixedCut, which only keeps the information about the mix; actual mixing is performed
+        during the call to `load_features`.
+        """
         return self.overlay(other=other, offset_other_by=self.duration, snr=snr)
 
 
@@ -93,14 +129,16 @@ class MixedCut:
     snr: Decibels
 
     def with_cut_set(self, cut_set: 'CutSet') -> 'MixedCut':
-        # TODO: temporary workaround; think how to design this part better
-        #  maybe make MixedCut "own" the Cuts that it consists of (but then the manifests are gonna be really thick)
-        #  or just pass the CutSet object in here which is not elegant but maybe better than this
+        """
+        Provide the source cut set that can be looked up to resolve the MixedCut's dependencies.
+        This method is a necessary, because the MixedCut acts like a "pointer" to the cuts that were used to create it.
+        """
         self._cut_set = cut_set
         return self
 
     @property
     def supervisions(self) -> List[SupervisionSegment]:
+        """Lists the supervisions of the underyling source cuts."""
         return self._cut_set.cuts[self.left_cut_id].supervisions + self._cut_set.cuts[self.right_cut_id].supervisions
 
     @property
@@ -111,6 +149,7 @@ class MixedCut:
         )
 
     def load_features(self, root_dir: Optional[Pathlike] = None) -> np.ndarray:
+        """Loads the features of the source cuts and overlays them on-the-fly."""
         cuts = [self._cut_set.cuts[id_] for id_ in [self.left_cut_id, self.right_cut_id]]
         frame_length, frame_shift = cuts[0].features.frame_length, cuts[0].features.frame_shift
         assert frame_length == cuts[1].features.frame_length
@@ -176,6 +215,10 @@ class CutSet:
             yaml.safe_dump([{**asdict_nonull(cut), 'type': type(cut).__name__} for cut in self], stream=f)
 
     def with_source_cuts_from(self, source: 'CutSet'):
+        """
+        Provide the source cut set that can be looked up to resolve the dependencies of the MixedCut's in this CutSet.
+        This method is a necessary, because the MixedCut acts like a "pointer" to the cuts that were used to create it.
+        """
         for cut in self.mixed_cuts.values():
             cut.with_cut_set(source)
         return self
@@ -191,6 +234,10 @@ class CutSet:
 
 
 def make_cuts_from_supervisions(supervision_set: SupervisionSet, feature_set: FeatureSet) -> CutSet:
+    """
+    Utility that converts a SupervisionSet to a CutSet without any adjustment of the segment boundaries.
+    It attaches the relevant features from the corresponding FeatureSet.
+    """
     cuts = (
         Cut(
             id=str(uuid4()),
@@ -211,6 +258,18 @@ def make_cuts_from_supervisions(supervision_set: SupervisionSet, feature_set: Fe
 
 
 def mix_stereo_cut_set(cut_set: CutSet) -> CutSet:
+    """
+    Utility that converts a CutSet that contains Cuts from both channels of a recording into a CutSet where
+    these channels are downmixed into mono (by overlaying their features with 0 SNR and 0 offset).
+    It assumes the CutSet is sorted by a tuple of (recording_id, channel); conceptually equivalent to:
+
+    [
+        Cut(channel=0, supervisions=[SupervisionSegment(recording_id='abcd', ...), ...], ...),
+        Cut(channel=1, supervisions=[SupervisionSegment(recording_id='abcd', ...), ...], ...),
+        Cut(channel=0, supervisions=[SupervisionSegment(recording_id='efgh', ...), ...], ...),
+        Cut(channel=1, supervisions=[SupervisionSegment(recording_id='efgh', ...), ...], ...),
+    ]
+    """
     channels = list(set(c.channel for c in cut_set))
     channel0_cuts = [c for c in cut_set if c.channel == channels[0]]
     channel1_cuts = [c for c in cut_set if c.channel == channels[1]]
--- lhotse/dataset/source_separation.py
+++ lhotse/dataset/source_separation.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Optional
 
 import torch
 from torch.utils.data import Dataset
@@ -9,12 +9,18 @@ from lhotse.utils import Pathlike
 
 
 class SourceSeparationDataset(Dataset):
-    def __init__(self, sources_set: CutSet, mixtures_set: CutSet, root_dir: Pathlike):
+    """
+    A PyTorch Dataset for the source separation task. It's created from two CutSets - one provides the audio
+    cuts for the sources, and the other one the audio cuts for the signal mix. When queried for data samples,
+    it returns a dict of {'sources': tensor, 'mixture': tensor}.
+    """
+
+    def __init__(self, sources_set: CutSet, mixtures_set: CutSet, root_dir: Optional[Pathlike] = None):
         super().__init__()
         self.sources_set = sources_set
         self.mixtures_set = mixtures_set
         self.cut_ids = list(self.mixtures_set.cuts.keys())
-        self.root_dir = Path(root_dir)
+        self.root_dir = Path(root_dir) if root_dir else None
         assert set(self.cut_ids) == set(self.mixtures_set.cuts.keys())
 
     def __getitem__(self, idx: int) -> Dict[str, torch.Tensor]:
