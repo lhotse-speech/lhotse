20ff8afc437a5cbe8c554e079bc167d139243cfc
Junbo Zhang <zhangjunbo1@xiaomi.com>
2020-07-18 13:07:22 +0800

    Increase testing coverage

--- test/cut/test_cut.py
+++ test/cut/test_cut.py
@@ -2,7 +2,14 @@ from math import isclose
 import pytest
 
 from lhotse.audio import RecordingSet
-from lhotse.cut import CutSet, make_cuts_from_recordings
+from lhotse.features import FeatureSet
+from lhotse.supervision import SupervisionSet
+from lhotse.cut import (
+    CutSet,
+    make_cuts_from_recordings,
+    make_cuts_from_supervisions_features,
+    make_cuts_from_supervisions_recordings
+)
 
 
 @pytest.fixture()
@@ -16,6 +23,16 @@ def libri_cut_set():
 
 
 @pytest.fixture
+def libri_features_set():
+    return FeatureSet.from_yaml('test/fixtures/libri/feature_manifest.yml')
+
+
+@pytest.fixture
+def supervision_set():
+    return SupervisionSet.from_yaml('test/fixtures/supervision.yml')
+
+
+@pytest.fixture
 def libri_cut(libri_cut_set):
     return libri_cut_set['849e13d8-61a2-4d09-a542-dac1aee1b544']
 
@@ -26,11 +43,10 @@ def test_load_audio(libri_cut, libri_recording_set):
     assert samples.shape[1] == 10 * 16000  # samples count = duration * sampling_rate
 
 
-def test_load_audio(libri_cut):
+def test_load_audio_by_self(libri_cut):
     # Same as test_load_audio(libri_cut, libri_recording_set), but do not provide external recording_set
     samples = libri_cut.load_audio()
-    assert samples.shape[0] == 1
-    assert samples.shape[1] == 10 * 16000
+    assert samples.shape == (1, 10 * 16000)
 
 
 def test_num_frames(libri_cut):
@@ -47,8 +63,24 @@ def test_load_features(libri_cut):
     assert feats.shape[1] == libri_cut.features.num_features
 
 
+def test_load_none_features(libri_cut):
+    libri_cut.features = None
+    feats = libri_cut.load_features()
+    assert feats is None
+
+
 def test_make_cuts_from_recordings(libri_recording_set):
     expected_duration = 16.04
     cutset = make_cuts_from_recordings(libri_recording_set)
     duration = list(cutset.cuts.values())[0].duration
     assert isclose(duration, expected_duration)
+
+
+def test_make_cuts_from_supervisions_features(supervision_set, libri_features_set):
+    cutset = make_cuts_from_supervisions_features(supervision_set, libri_features_set)
+    assert len(cutset) == 2
+
+
+def test_make_cuts_from_supervisions_recordingss(supervision_set, libri_recording_set):
+    cutset = make_cuts_from_supervisions_recordings(supervision_set, libri_recording_set)
+    assert len(cutset) == 2
