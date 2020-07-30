91dab525de4d50b15b348bb643d1600d8676b7dd
Piotr Żelasko <petezor@gmail.com>
2020-07-30 13:48:28 -0400

    On-the-fly feature extraction dataset example

--- test/dataset/test_unsupervised_dataset.py
+++ test/dataset/test_unsupervised_dataset.py
@@ -1,7 +1,10 @@
+import numpy as np
 import pytest
 
 from lhotse.cut import CutSet
 from lhotse.dataset import UnsupervisedDataset, UnsupervisedWaveformDataset
+from lhotse.dataset.unsupervised import OnTheFlyFeatureExtractionUnsupervisedDataset
+from lhotse.features import Fbank
 
 
 @pytest.fixture
@@ -21,3 +24,26 @@ def test_unsupervised_waveform_dataset(libri_cut_set):
     assert len(dataset) == 1
     audio = dataset[0]
     assert audio.shape == (1, 10 * 16000)
+
+
+def test_on_the_fly_feature_extraction_unsupervised_dataset(libri_cut_set):
+    ref_dataset = UnsupervisedDataset(libri_cut_set)
+    tested_dataset = OnTheFlyFeatureExtractionUnsupervisedDataset(
+        feature_extractor=Fbank(),
+        cuts=libri_cut_set
+    )
+    ref_feats = ref_dataset[0]
+    tested_feats = tested_dataset[0]
+    # Note: comparison to 1 decimal fails.
+    #       I'm assuming this is due to lilcom's compression.
+    #       Pytest outputs looks like the following:
+    # E       Mismatched elements: 4 / 23000 (0.0174%)
+    # E       Max absolute difference: 0.46469784
+    # E       Max relative difference: 0.6171043
+    # E        x: array([[-11.5, -11.4,  -9.9, ...,  -5.5,  -6.5,  -7.4],
+    # E              [-13.2, -11.2,  -9.6, ...,  -5.6,  -6.5,  -7.6],
+    # E              [-12. , -10.1, -10.1, ...,  -5.8,  -7. ,  -7.8],...
+    # E        y: array([[-11.5, -11.4,  -9.9, ...,  -5.5,  -6.5,  -7.4],
+    # E              [-13.2, -11.2,  -9.6, ...,  -5.6,  -6.5,  -7.6],
+    # E              [-12. , -10.1, -10.1, ...,  -5.8,  -7. ,  -7.8],...
+    np.testing.assert_array_almost_equal(ref_feats, tested_feats, decimal=0)
