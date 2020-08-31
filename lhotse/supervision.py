3eedeb3c290c844b8e28b1ddeb4cc3a8c6a9f867
Piotr Żelasko <petezor@gmail.com>
2020-08-31 13:09:03 -0400

    Fix typing.Dict usage

--- lhotse/supervision.py
+++ lhotse/supervision.py
@@ -1,5 +1,5 @@
 from dataclasses import asdict, dataclass, field
-from typing import Callable, Dict, Iterable, Optional
+from typing import Callable, Dict, Iterable, Optional, Any
 
 from lhotse.utils import Pathlike, Seconds, asdict_nonull, load_yaml, save_to_yaml
 
@@ -15,7 +15,7 @@ class SupervisionSegment:
     language: Optional[str] = None
     speaker: Optional[str] = None
     gender: Optional[str] = None
-    custom: Dict[str] = field(default_factory=dict)
+    custom: Dict[str, Any] = field(default_factory=dict)
 
     @property
     def end(self) -> Seconds:
