0ce59f45eac530afe0872ca6fa872fc1c1afffd7
Piotr Żelasko <petezor@gmail.com>
2020-10-29 10:57:03 -0400

    Reworked K2 dataset according to comments

--- lhotse/dataset/speech_recognition.py
+++ lhotse/dataset/speech_recognition.py
@@ -1,8 +1,8 @@
-from itertools import chain
-from typing import Dict
+from typing import Dict, Sequence
 
 import torch
 from torch.utils.data import Dataset
+from torch.utils.data.dataloader import DataLoader
 
 from lhotse.cut import CutSet
 
@@ -62,9 +62,6 @@ class SpeechRecognitionDataset(Dataset):
 
 class K2SpeechRecognitionDataset(Dataset):
     """
-    .. warning::
-        This is just a draft of how Lhotse can interact with K2 - it's under active development.
-
     The PyTorch Dataset for the speech recognition task using K2 library.
     Each item in this dataset is a dict of:
 
@@ -74,106 +71,90 @@ class K2SpeechRecognitionDataset(Dataset):
             'features': (T x F) tensor,
             'supervisions': List[Dict] -> [
                 {
+                    'example_idx': int
                     'text': string,
-                    'fsa': k2.Fsa,
                     'start_frame': int,
                     'end_frame': int
                 } (multiplied N times, for each of the N supervisions present in the Cut)
-            ],
-            'supervisions_mask': (T) tensor
+            ]
         }
 
-    The ``supervisions_mask`` field is a mask that specifies which frames are covered by a supervision
-    by assigning a value of 1 (in this case: segments with transcribed speech contents),
-    and which are not by asigning a value of 0 (in this case: padding, contextual noise,
-    or in general the acoustic context without transcription).
+    The 'example_idx' field is the index of the Cut used to create the example in the Dataset.
+    It is mapped to the batch index later in the DataLoader.
     """
 
     def __init__(self, cuts: CutSet):
-        import k2
         super().__init__()
         self.cuts = cuts
         self.cut_ids = list(self.cuts.ids)
 
-        # We're creating a vocabulary of all characters present in the training CutSet.
-        # To do that we iterate through all the supervisions, and use chain.from_iterable
-        # to convert a sequence of their 'text' fields into a flat iterable of characters.
-
-        # Add special symbols first
-        self.vocab = ['<eps>', '<sil>', '<unk>'] + sorted(
-            # Add symbols present in the corpus
-            set(
-                chain.from_iterable(
-                    list(supervision.text)
-                    for cut in self.cuts
-                    for supervision in cut.supervisions
-                )
-            )
-        )
-        self.symbol_table = k2.SymbolTable(
-            _id2sym={i: tok for i, tok in enumerate(self.vocab)},
-            _sym2id={tok: i for i, tok in enumerate(self.vocab)}
-        )
-
     def __getitem__(self, idx: int) -> Dict[str, torch.Tensor]:
-        import k2
         cut_id = self.cut_ids[idx]
         cut = self.cuts[cut_id]
 
         features = torch.from_numpy(cut.load_features())
-        mask = torch.from_numpy(cut.supervisions_feature_mask())
-
-        sil_id = self.symbol_table.get('<sil>')
-        eps_id = self.symbol_table.get('<eps>')
-
-        supervision_fsas = []
-        for supervision in cut.supervisions:
-            # TODO(pzelasko): I am not sure how to attach the symbol table to the FSA object
-            #                 (or whether that's need at all?)
-
-            # Creating the core supervision FSA corresponding to the supervision text.
-            fsa = k2.linear_fsa([self.symbol_table.get(c) for c in supervision.text])
-
-            # TODO: Adding optional silence at beginning and end.
-
-            # TODO(pzelasko): Should be possible to create it with sth like:
-            #   silfsa = k2.union(
-            #     k2.linear_fsa([sil_id]), k2.linear_fsa([eps_id])
-            #   ).closure()
-            silfsa = k2.Fsa.from_str(f'''0 1 {sil_id} 0
-                                         0 1 {eps_id} 0
-                                         1 0 {sil_id} 0
-                                         1''')
-            # TODO(pzelasko): is it possible to do:
-            #   training_fsa = silfsa + fsa + silfsa
-            #  or:
-            #   training_fsa = k2.concat(silfsa, fsa, silfsa)
-            #  ?
-            supervision_fsas.append(fsa)
-
-        # TODO(pzelasko): I think 'supervision_fsas' could be "joined" (connected) with
-        #                 some sort of self-looped union of <eps>, <sil> and <unk>
-        #                 (or maybe a sigma star, i.e. the full vocabulary) in between...
 
         return {
             'features': features,
             'supervisions': [
                 {
+                    'example_idx': idx,
                     'text': sup.text,
-                    'fsa': fsa,
                     'start_frame': round(sup.start / cut.frame_shift),
                     'end_frame': round(sup.end / cut.frame_shift),
-                } for sup, fsa in zip(
-                    # CutSet's supervisions can exceed the cut, when the cut starts/ends in the middle
-                    # of a supervision (they would have relative times e.g. -2 seconds start, meaning
-                    # it started 2 seconds before the Cut starts). We use s.trim() to get rid of that
-                    # property, ensuring the supervision time span does not exceed that of the cut.
-                    (s.trim(cut.duration) for s in cut.supervisions),
-                    supervision_fsas
-                )
-            ],
-            'supervisions_mask': mask
+                }
+                # CutSet's supervisions can exceed the cut, when the cut starts/ends in the middle
+                # of a supervision (they would have relative times e.g. -2 seconds start, meaning
+                # it started 2 seconds before the Cut starts). We use s.trim() to get rid of that
+                # property, ensuring the supervision time span does not exceed that of the cut.
+                for sup in (s.trim(cut.duration) for s in cut.supervisions)
+            ]
         }
 
     def __len__(self) -> int:
         return len(self.cut_ids)
+
+
+class K2DataLoader(DataLoader):
+    """
+    A PyTorch DataLoader that has a custom collate_fn that complements the K2SpeechRecognitionDataset.
+
+    The 'features' tensor is collated in a standard way to return a tensor of shape (B, T, F).
+
+    The 'supervisions' dict contains each supervision as a separate entry
+    (its len() might be more than B dim in 'features').
+    The 'example_idx' field which originally points to index of the example in the Dataset
+    is remapped to the index of the corresponding features matrix in the collated 'features'.
+    Multiple supervisions coming from the same cut will share the same 'example_idx'.
+    """
+
+    def __init__(self, *args, **kwargs):
+        if 'collate_fn' in kwargs:
+            raise ValueError('Cannot override collate_fn in K2DataLoader.')
+        super().__init__(*args, collate_fn=multi_supervision_collate_fn, **kwargs)
+
+
+def multi_supervision_collate_fn(batch: Sequence[Dict]) -> Dict:
+    """Custom collate_fn for K2SpeechRecognitionDataset. See its docs for details."""
+    from torch.utils.data._utils.collate import default_collate
+
+    dataset_idx_to_batch_idx = {
+        example['supervisions'][0]['example_idx']: batch_idx
+        for batch_idx, example in enumerate(batch)
+    }
+
+    def update(d: Dict, **kwargs) -> Dict:
+        for key, value in kwargs.items():
+            d[key] = value
+        return d
+
+    supervisions = default_collate([
+        update(sup, example_idx=dataset_idx_to_batch_idx[sup['example_idx']])
+        for example in batch
+        for sup in example['supervisions']
+    ])
+    feats = default_collate([example['features'] for example in batch])
+    return {
+        'features': feats,
+        'supervisions': supervisions
+    }
--- test/dataset/test_speech_recognition_dataset.py
+++ test/dataset/test_speech_recognition_dataset.py
@@ -2,6 +2,7 @@ import pytest
 
 from lhotse.cut import CutSet
 from lhotse.dataset import SpeechRecognitionDataset
+from lhotse.dataset.speech_recognition import K2DataLoader, K2SpeechRecognitionDataset
 
 
 @pytest.fixture
@@ -17,3 +18,51 @@ def test_speech_recognition_dataset(libri_cut_set):
     assert item['features'].shape == (154, 80)
     assert item['text'] == "IN EIGHTEEN THIRTEEN"
     assert item['supervisions_mask'].shape == (154,)
+
+
+@pytest.fixture
+def k2_cut_set(libri_cut_set):
+    # Create a cut set with 4 cuts, one of them having two supervisions
+    return CutSet.from_cuts([
+        libri_cut_set[0],
+        libri_cut_set[0].with_id('copy-1'),
+        libri_cut_set[0].with_id('copy-2'),
+        libri_cut_set[0].append(libri_cut_set[0])
+    ]).pad()
+
+
+def test_k2_speech_recognition_dataset(k2_cut_set):
+    dataset = K2SpeechRecognitionDataset(k2_cut_set)
+    for i in range(3):
+        example = dataset[i]
+        assert example['features'].shape == (308, 80)
+        assert len(example['supervisions']) == 1
+        assert example['supervisions'][0]['text'] == 'IN EIGHTEEN THIRTEEN'
+        assert example['supervisions'][0]['example_idx'] == i
+        assert example['supervisions'][0]['start_frame'] == 0
+        assert example['supervisions'][0]['end_frame'] == 154
+    example = dataset[3]
+    assert example['features'].shape == (308, 80)
+    assert len(example['supervisions']) == 2
+    assert example['supervisions'][0]['text'] == 'IN EIGHTEEN THIRTEEN'
+    assert example['supervisions'][0]['example_idx'] == 3
+    assert example['supervisions'][0]['start_frame'] == 0
+    assert example['supervisions'][0]['end_frame'] == 154
+    assert example['supervisions'][1]['text'] == 'IN EIGHTEEN THIRTEEN'
+    assert example['supervisions'][1]['example_idx'] == 3
+    assert example['supervisions'][1]['start_frame'] == 154
+    assert example['supervisions'][1]['end_frame'] == 308
+
+
+def test_k2_dataloader(k2_cut_set):
+    from torch import tensor
+    dataset = K2SpeechRecognitionDataset(k2_cut_set)
+    dloader = K2DataLoader(dataset, batch_size=4)
+    batch = next(iter(dloader))
+    assert batch['features'].shape == (4, 308, 80)
+    # Each list has 5 items, to account for:
+    # one cut with two supervisions + 3 three cuts with one supervision
+    assert (batch['supervisions']['example_idx'] == tensor([0, 1, 2, 3, 3])).all()
+    assert batch['supervisions']['text'] == ['IN EIGHTEEN THIRTEEN'] * 5  # a list, not tensor
+    assert (batch['supervisions']['start_frame'] == tensor([0] * 4 + [154])).all()
+    assert (batch['supervisions']['end_frame'] == tensor([154] * 4 + [308])).all()
