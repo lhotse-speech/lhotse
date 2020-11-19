e4d039d8159b486170ffc5513776db3cf02359e2
Piotr Żelasko <petezor@gmail.com>
2020-11-19 15:51:30 -0500

    First take at exporting Lhotse manifests to Kaldi data dir

--- lhotse/kaldi.py
+++ lhotse/kaldi.py
@@ -1,7 +1,8 @@
 from collections import defaultdict
 from pathlib import Path
-from typing import Dict, Optional, Tuple
+from typing import Any, Dict, Optional, Tuple
 
+from lhotse import CutSet
 from lhotse.audio import AudioSource, Recording, RecordingSet
 from lhotse.supervision import SupervisionSegment, SupervisionSet
 from lhotse.utils import Pathlike
@@ -78,6 +79,77 @@ def load_kaldi_data_dir(path: Pathlike, sampling_rate: int) -> Tuple[RecordingSe
     return audio_set, supervision_set
 
 
+def export_to_kaldi(recordings: RecordingSet, supervisions: SupervisionSet, output_dir: Pathlike):
+    """
+    Export a pair of ``RecordingSet`` and ``SupervisionSet`` to a Kaldi data directory.
+    Currently, it only supports single-channel recordings that have a single ``AudioSource``.
+
+    The ``RecordingSet`` and ``SupervisionSet`` must be compatible, i.e. it must be possible to create a
+    ``CutSet`` out of them.
+
+    :param recordings: a ``RecordingSet`` manifest.
+    :param supervisions: a ``SupervisionSet`` manifest.
+    :param output_dir: path where the Kaldi-style data directory will be created.
+    """
+    output_dir = Path(output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    assert all(len(r.sources) == 1 for r in recordings), "Kaldi export of Recordings with multiple audio sources " \
+                                                         "is currently not supported."
+    assert all(r.num_channels == 1 for r in recordings), "Kaldi export of multi-channel Recordings is currently " \
+                                                         "not supported."
+
+    # Create a simple CutSet that ties together the recording <-> supervision information.
+    cuts = CutSet.from_manifests(recordings=recordings, supervisions=supervisions)
+
+    # wav.scp
+    save_kaldi_text_mapping(
+        data={
+            recording.id: f'{source.source} |' if source.type == 'command' else source.source
+            for recording in recordings
+            for src_idx, source in enumerate(recording.sources)
+        },
+        path=output_dir / 'wav.scp'
+    )
+    # segments
+    save_kaldi_text_mapping(
+        data={cut.supervisions[0].id: f'{cut.recording_id} {cut.start} {cut.end}' for cut in cuts},
+        path=output_dir / 'segments'
+    )
+    # text
+    save_kaldi_text_mapping(
+        data={cut.supervisions[0].id: cut.supervisions[0].text for cut in cuts},
+        path=output_dir / 'text'
+    )
+    # utt2spk
+    save_kaldi_text_mapping(
+        data={cut.supervisions[0].id: cut.supervisions[0].speaker for cut in cuts},
+        path=output_dir / 'utt2spk'
+    )
+    # utt2dur
+    save_kaldi_text_mapping(
+        data={cut.supervisions[0].id: cut.duration for cut in cuts},
+        path=output_dir / 'utt2dur'
+    )
+    # reco2dur
+    save_kaldi_text_mapping(
+        data={recording.id: recording.duration for recording in recordings},
+        path=output_dir / 'reco2dur'
+    )
+    # utt2lang [optional]
+    if all(s.language is not None for s in supervisions):
+        save_kaldi_text_mapping(
+            data={cut.supervisions[0].id: cut.supervisions[0].language for cut in cuts},
+            path=output_dir / 'utt2lang'
+        )
+    # utt2gender [optional]
+    if all(s.gender is not None for s in supervisions):
+        save_kaldi_text_mapping(
+            data={cut.supervisions[0].id: cut.supervisions[0].gender for cut in cuts},
+            path=output_dir / 'utt2gender'
+        )
+
+
 def load_kaldi_text_mapping(path: Path, must_exist: bool = False) -> Dict[str, Optional[str]]:
     """Load Kaldi files such as utt2spk, spk2gender, text, etc. as a dict."""
     mapping = defaultdict(lambda: None)
@@ -87,3 +159,10 @@ def load_kaldi_text_mapping(path: Path, must_exist: bool = False) -> Dict[str, O
     elif must_exist:
         raise ValueError(f"No such file: {path}")
     return mapping
+
+
+def save_kaldi_text_mapping(data: Dict[str, Any], path: Path):
+    """Save flat dicts to Kaldi files such as utt2spk, spk2gender, text, etc."""
+    with path.open('w') as f:
+        for key, value in data.items():
+            print(key, value, file=f)
