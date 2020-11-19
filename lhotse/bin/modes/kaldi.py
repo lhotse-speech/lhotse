b0574f0bc95c84212d1d260e3d23239f112194c0
Piotr Żelasko <petezor@gmail.com>
2020-11-19 16:00:35 -0500

    Add a CLI for NSC data prep and Kaldi export

--- lhotse/bin/modes/kaldi.py
+++ lhotse/bin/modes/kaldi.py
@@ -2,18 +2,23 @@ from pathlib import Path
 
 import click
 
+from lhotse import load_manifest
 from lhotse.bin.modes.cli_base import cli
-from lhotse.kaldi import load_kaldi_data_dir
+from lhotse.kaldi import export_to_kaldi, load_kaldi_data_dir
 from lhotse.utils import Pathlike
 
-__all__ = ['convert_kaldi']
+
+@cli.group()
+def kaldi():
+    """Kaldi import/export related commands."""
+    pass
 
 
-@cli.command()
+@kaldi.command(name='import')
 @click.argument('data_dir', type=click.Path(exists=True, file_okay=False))
 @click.argument('sampling_rate', type=int)
 @click.argument('manifest_dir', type=click.Path())
-def convert_kaldi(data_dir: Pathlike, sampling_rate: int, manifest_dir: Pathlike):
+def import_(data_dir: Pathlike, sampling_rate: int, manifest_dir: Pathlike):
     """
     Convert a Kaldi data dir DATA_DIR into a directory MANIFEST_DIR of lhotse manifests. Ignores feats.scp.
     The SAMPLING_RATE has to be explicitly specified as it is not available to read from DATA_DIR.
@@ -24,3 +29,18 @@ def convert_kaldi(data_dir: Pathlike, sampling_rate: int, manifest_dir: Pathlike
     recording_set.to_json(manifest_dir / 'audio.json')
     if maybe_supervision_set is not None:
         maybe_supervision_set.to_json(manifest_dir / 'supervision.json')
+
+
+@kaldi.command()
+@click.argument('recordings', type=click.Path(exists=True, dir_okay=False))
+@click.argument('supervisions', type=click.Path(exists=True, dir_okay=False))
+@click.argument('output_dir', type=click.Path())
+def export(recordings: Pathlike, supervisions: Pathlike, output_dir: Pathlike):
+    """
+    Convert a pair of ``RecordingSet`` and ``SupervisionSet`` manifests into a Kaldi-style data directory.
+    """
+    export_to_kaldi(
+        recordings=load_manifest(recordings),
+        supervisions=load_manifest(supervisions),
+        output_dir=output_dir
+    )
