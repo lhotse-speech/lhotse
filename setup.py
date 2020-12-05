44b0909721aec46269974a5817396b64c9467394
Piotr Żelasko <petezor@gmail.com>
2020-12-04 21:49:32 -0500

    Remove Python 3.9 support (was worth trying)

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Intended Audience :: Science/Research",
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS :: MacOS X",
