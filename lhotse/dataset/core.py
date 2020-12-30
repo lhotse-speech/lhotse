Various fixes to num frames/samples computation + randomized testing

lhotse/dataset/core.py
@@ -210,11 +210,12 @@ class SpeechDataset(torch.utils.data.IterableDataset):
                             info.update(entry)
                     supervisions.append(info)
             # At this point we transformed all the supervisions into fields:
-            # we have to collate them now.
-            output['supervisions'] = default_collate(supervisions)
-            output['supervisions'].update(
-                {key: collaters[key].collate(values, key=key) for key, values in custom_collated.items()}
-            )
+            # we have to collate them now (assuming there are any).
+            if supervisions:
+                output['supervisions'] = default_collate(supervisions)
+                output['supervisions'].update(
+                    {key: collaters[key].collate(values, key=key) for key, values in custom_collated.items()}
+                )
 
         return output
 
@@ -273,7 +274,7 @@ class SpeechDataset(torch.utils.data.IterableDataset):
     def _validate(self) -> None:
         for cut in self.cuts:
             for supervision in cut.supervisions:
-                assert cut.start <= supervision.start <= supervision.end <= cut.end, \
+                assert (cut.start - 1e-5) <= supervision.start <= supervision.end <= (cut.end + 1e-5), \
                     f"Cutting in the middle of a supervision is currently not supported for the ASR task. " \
                     f"Cut ID violating the pre-condition: '{cut.id}'"
         assert self.max_frames > 0

lhotse/dataset/fields.py
@@ -1,3 +1,5 @@
+from decimal import ROUND_FLOOR, ROUND_HALF_DOWN
+
 import torch
 from torch.utils.data.dataloader import default_collate
 from typing import Any, Dict, List, Sequence
@@ -5,7 +7,7 @@ from typing_extensions import Protocol, runtime_checkable
 
 from lhotse import CutSet, SupervisionSegment
 from lhotse.cut import AnyCut, MixedCut
-from lhotse.utils import Seconds, compute_num_frames
+from lhotse.utils import Seconds, compute_num_frames, compute_num_samples
 
 
 class SignalField(Protocol):
@@ -44,25 +46,33 @@ class RequiresCustomCollation(Protocol):
 class FeatureSpan:
     def __call__(self, supervision: SupervisionSegment, cut: AnyCut) -> Dict[str, Any]:
         return {
-            'start_frame': compute_num_frames(supervision.start, cut.frame_shift),
-            'num_frames': compute_num_frames(supervision.duration, frame_shift=cut.frame_shift)
-            # TODO: try this:
-            # 'num_frames': compute_num_frames(supervision.end, cut.frame_shift) - compute_num_frames(supervision.start, cut.frame_shift)
-            # Original version
-            # 'num_frames': _asserted_num_frames(
-            #     output['features'].shape[2 if self.multi_channel else 1],
-            #     supervision.start,
-            #     supervision.duration,
-            #     cut.frame_shift
-            # )
+            'start_frame': compute_num_frames(
+                supervision.start,
+                frame_shift=cut.frame_shift,
+                # Note: Rounding "floor" can sometimes result in one extra frame being included
+                # in the left context; but it guarantees that we will never go out-of-bounds when
+                # summing start_frame + num_frames.
+                rounding=ROUND_FLOOR
+            ),
+            'num_frames': compute_num_frames(
+                supervision.duration,
+                frame_shift=cut.frame_shift
+            )
         }
 
 
 class AudioSpan:
     def __call__(self, supervision: SupervisionSegment, cut: AnyCut) -> Dict[str, Any]:
         return {
-            'start_sample': round(supervision.start * cut.sampling_rate),
-            'num_samples': round(supervision.duration * cut.sampling_rate)
+            'start_sample': compute_num_samples(
+                supervision.start,
+                sampling_rate=cut.sampling_rate,
+                rounding=ROUND_FLOOR
+            ),
+            'num_samples': compute_num_samples(
+                supervision.duration,
+                sampling_rate=cut.sampling_rate
+            )
         }
 
 