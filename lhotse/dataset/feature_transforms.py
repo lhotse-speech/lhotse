fb49636e844daeec2a998f88abd990cd2ade8910
Jan Vainer <vainerjan@gmail.com>
2021-03-15 21:14:16 +0100

    Refactor Standardize to GlobalMVN
    Compute stats from CutSet, or load stats from file.
    Save stats to disk with torch.save(global_mvn.state_dict())

--- lhotse/dataset/feature_transforms.py
+++ lhotse/dataset/feature_transforms.py
@@ -1,15 +1,30 @@
 import torch
 
+from typing import Union
+
 from lhotse import CutSet
+from lhotse.utils import Pathlike
 
 
-class Standardize:
-    def __init__(self, cuts: CutSet):
+class GlobalMVN(torch.nn.Module):
+    """Apply global mean and variance normalization"""
+
+    def __init__(self, norm_means, norm_stds):
+        super().__init__()
+        self.register_buffer("norm_means", torch.as_tensor(norm_means))
+        self.register_buffer("norm_stds", torch.as_tensor(norm_stds))
+
+    @classmethod
+    def from_cuts(cls, cuts: CutSet) -> "GlobalMVN":
         stats = cuts.compute_global_feature_stats()
-        self.norm_means = stats["norm_means"]
-        self.norm_stds = stats["norm_stds"]
+        return cls(stats["norm_means"], stats["norm_stds"])
 
-    def __call__(self, features: torch.Tensor) -> torch.Tensor:
+    @classmethod
+    def from_file(cls, stats_file: Pathlike) -> "GlobalMVN":
+        stats = torch.load(stats_file)
+        return cls(stats["norm_means"], stats["norm_stds"])
+
+    def forward(self, features: torch.Tensor) -> torch.Tensor:
         return (features - self.norm_means) / self.norm_stds
 
     def inverse(self, features: torch.Tensor) -> torch.Tensor:
