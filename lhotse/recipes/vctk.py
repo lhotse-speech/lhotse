869cb0f6d6d5c25b6de745ce1b8985f9d7c6f6a9
Jan Vainer <vainerjan@gmail.com>
2021-04-09 14:48:17 +0200

    Wrap output_dir in vctk recipe to Path

--- lhotse/recipes/vctk.py
+++ lhotse/recipes/vctk.py
@@ -175,6 +175,7 @@ def prepare_vctk(
     validate_recordings_and_supervisions(recordings, supervisions)
 
     if output_dir is not None:
+        output_dir = Path(output_dir)
         output_dir.mkdir(parents=True, exist_ok=True)
         recordings.to_json(output_dir / 'recordings.json')
         supervisions.to_json(output_dir / 'supervisions.json')
