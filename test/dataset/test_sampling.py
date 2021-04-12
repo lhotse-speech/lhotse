02621f9a07ab5f9f9b5e886ae1d25ab7f255bcf5
Piotr Żelasko <petezor@gmail.com>
2021-04-12 17:24:50 -0400

    Ensure equal number of batches across all distributed workers in an epoch

--- test/dataset/test_sampling.py
+++ test/dataset/test_sampling.py
@@ -494,9 +494,10 @@ def test_bucketing_sampler_time_constraints(constraint):
     assert set(cut_set.ids) == set(cut_ids)
 
 
+@pytest.mark.parametrize('epoch', [0, 1, 2])
 @pytest.mark.parametrize('world_size', [2, 3, 4])
 @pytest.mark.parametrize('n_cuts', [995, 996, 997, 998, 999, 1000, 1001, 1002, 1003])
-def test_partitions_are_equal(world_size, n_cuts):
+def test_partitions_are_equal(epoch, world_size, n_cuts):
     cut_set = DummyManifest(CutSet, begin_id=0, end_id=n_cuts)
     for c in cut_set:
         c.duration += (10 * random.random())
@@ -504,6 +505,8 @@ def test_partitions_are_equal(world_size, n_cuts):
         SingleCutSampler(cut_set, max_duration=25.0, rank=i, world_size=world_size)
         for i in range(world_size)
     ]
+    for s in samplers:
+        s.set_epoch(epoch)
     n_batches = [len(s) for s in samplers]
     print(n_batches)
     assert all(nb == n_batches[0] for nb in n_batches)
