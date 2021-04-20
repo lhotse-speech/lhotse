970e55b060a7dce3da15f33de069f6168350377f
Piotr Żelasko <petezor@gmail.com>
2021-04-20 08:41:58 -0400

    Fix speaker ID in CallHome English

--- lhotse/recipes/callhome_english.py
+++ lhotse/recipes/callhome_english.py
@@ -139,7 +139,7 @@ def read_rttm(path: Pathlike) -> SupervisionSet:
                 start=start,
                 duration=duration,
                 channel=channel,
-                speaker=speaker,
+                speaker=f'{recording_id}_{speaker}',
                 language='English'
             )
         )
