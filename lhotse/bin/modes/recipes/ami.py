ea2ef9d2f0d4c59cb8b9bf229722b32a6cc3d2a1
Desh Raj <r.desh26@gmail.com>
2021-05-11 20:30:37 -0400

    Fixed AMI CLI mode (#303)
    * initial commit for gale_arabic
    
    * Added GALE Mandarin recipe
    
    * Added language in SupervisionSegment
    
    * Added CMU Kids recipe
    
    * remove repeated corpora
    
    * Added CSLU kids recipe
    
    * minor change in CMU and CSLU kids bin
    
    * table of corpora in docs
    
    * added CSLU kids in docs
    
    * fix in CSLU kids comments
    
    * Adds mtedx recipe
    
    * Small fix to mtedx documentation
    
    * make AMI CLI mode consistent with python usage
    
    * removed mtedx scripts
    
    Co-authored-by: Matthew Wiesner <wiesner@jhu.edu>

--- lhotse/bin/modes/recipes/ami.py
+++ lhotse/bin/modes/recipes/ami.py
@@ -10,36 +10,89 @@ __all__ = ['ami']
 @prepare.command(context_settings=dict(show_default=True))
 @click.argument('corpus_dir', type=click.Path(exists=True, dir_okay=True))
 @click.argument('output_dir', type=click.Path())
-@click.option('-mic', type=click.Choice(['ihm','ihm-mix','sdm','mdm'], case_sensitive=False),
-              default='ihm', help='AMI microphone setting.')
-@click.option('-partition', type=click.Choice(['scenario-only','full-corpus','full-corpus-asr'],case_sensitive=False),
-              default='full-corpus-asr',
-              help='Data partition to use (see http://groups.inf.ed.ac.uk/ami/corpus/datasets.shtml).')
-@click.option('-max-pause', type=float, default=0.0, help='Max pause allowed between word segments to combine segments.')
+@click.option(
+    '--annotations',
+    type=click.Path(),
+    default=None,
+    help="Provide if annotations are download in a different directory than corpus.",
+)
+@click.option(
+    '--mic',
+    type=click.Choice(['ihm', 'ihm-mix', 'sdm', 'mdm'], case_sensitive=False),
+    default='ihm',
+    help='AMI microphone setting.',
+)
+@click.option(
+    '--partition',
+    type=click.Choice(
+        ['scenario-only', 'full-corpus', 'full-corpus-asr'], case_sensitive=False
+    ),
+    default='full-corpus-asr',
+    help='Data partition to use (see http://groups.inf.ed.ac.uk/ami/corpus/datasets.shtml).',
+)
+@click.option(
+    '--max-pause',
+    type=float,
+    default=0.0,
+    help='Max pause allowed between word segments to combine segments.',
+)
 def ami(
-        corpus_dir: Pathlike,
-        output_dir: Pathlike,
-        mic: str,
-        partition: str,
-        max_pause: float
+    corpus_dir: Pathlike,
+    output_dir: Pathlike,
+    annotations: Pathlike,
+    mic: str,
+    partition: str,
+    max_pause: float,
 ):
     """AMI data preparation."""
-    prepare_ami(corpus_dir, output_dir=output_dir, mic=mic, partition=partition, max_pause=max_pause)
+    prepare_ami(
+        corpus_dir,
+        annotations_dir=annotations,
+        output_dir=output_dir,
+        mic=mic,
+        partition=partition,
+        max_pause=max_pause,
+    )
 
 
 @obtain.command(context_settings=dict(show_default=True))
 @click.argument('target_dir', type=click.Path())
-@click.option('-mic', type=click.Choice(['ihm','ihm-mix','sdm','mdm'], case_sensitive=False),
-              default='ihm', help='AMI microphone setting.')
-@click.option('-url', type=str, default='http://groups.inf.ed.ac.uk/ami',
-              help='AMI data downloading URL.')
-@click.option('-force-download', type=bool, default=False,
-              help='If True, download even if file is present.')
+@click.option(
+    '--annotations',
+    type=click.Path(),
+    default=None,
+    help="To download annotations in a different directory than corpus.",
+)
+@click.option(
+    '--mic',
+    type=click.Choice(['ihm', 'ihm-mix', 'sdm', 'mdm'], case_sensitive=False),
+    default='ihm',
+    help='AMI microphone setting.',
+)
+@click.option(
+    '--url',
+    type=str,
+    default='http://groups.inf.ed.ac.uk/ami',
+    help='AMI data downloading URL.',
+)
+@click.option(
+    '--force-download',
+    type=bool,
+    default=False,
+    help='If True, download even if file is present.',
+)
 def ami(
-        target_dir: Pathlike,
-        mic: str,
-        url: str,
-        force_download: bool
+    target_dir: Pathlike,
+    annotations: Pathlike,
+    mic: str,
+    url: str,
+    force_download: bool,
 ):
     """AMI download."""
-    download_ami(target_dir, mic=mic, url=url, force_download=force_download)
+    download_ami(
+        target_dir,
+        annotations_dir=annotations,
+        mic=mic,
+        url=url,
+        force_download=force_download,
+    )
