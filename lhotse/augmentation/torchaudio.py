2242d2b1257af1bd26796547ccb819010f2ab539
Piotr Żelasko <petezor@gmail.com>
2021-06-01 15:15:41 -0400

    Fix edge cases when combining speed perturb, resampling, and cutting in the middle of a recording yields mismatched number of samples (#311)

--- lhotse/augmentation/torchaudio.py
+++ lhotse/augmentation/torchaudio.py
@@ -1,13 +1,14 @@
 import warnings
 from dataclasses import asdict, dataclass
-from typing import List, Union
+from decimal import ROUND_HALF_UP
+from typing import List, Optional, Tuple, Union
 
 import numpy as np
 import torch
 import torchaudio
 from packaging.version import parse as _version
 
-from lhotse.utils import during_docs_build
+from lhotse.utils import Seconds, compute_num_samples, during_docs_build, perturb_num_samples
 
 if not during_docs_build() and _version(torchaudio.__version__) < _version('0.7'):
     warnings.warn('Torchaudio SoX effects chains are only introduced in version 0.7 - '
@@ -97,8 +98,17 @@ class AudioTransform:
 
     Any ``AudioTransform`` can be used like a Python function, that expects two arguments:
     a numpy array of samples, and a sampling rate. E.g.:
-    >>> fn = AudioTransform.from_dict(...)
-    >>> new_audio = fn(audio, sampling_rate)
+
+        >>> fn = AudioTransform.from_dict(...)
+        >>> new_audio = fn(audio, sampling_rate)
+
+    Since we often use cuts of the original recording, they will refer to the timestamps
+    of the augmented audio (which might be speed perturbed and of different duration).
+    Each transform provides a helper method to recover the original audio timestamps:
+
+        >>> # When fn does speed perturbation:
+        >>> fn.reverse_timestamps(offset=5.055555, duration=10.1111111, sampling_rate=16000)
+        ... (5.0, 10.0)
 
     Furthermore, ``AudioTransform`` can be easily (de)serialized to/from dict
     that contains its name and parameters.
@@ -127,6 +137,25 @@ class AudioTransform:
         return AudioTransform.KNOWN_TRANSFORMS[data['name']](**data['kwargs'])
 
     def __call__(self, samples: np.ndarray, sampling_rate: int) -> np.ndarray:
+        """
+        Apply transform.
+
+        To be implemented in derived classes.
+        """
+        raise NotImplementedError
+
+    def reverse_timestamps(
+            self,
+            offset: Seconds,
+            duration: Optional[Seconds],
+            sampling_rate: int
+    ) -> Tuple[Seconds, Optional[Seconds]]:
+        """
+        Convert ``offset`` and ``duration`` timestamps to be adequate for the audio before the transform.
+        Useful for on-the-fly augmentation when a particular chunk of audio needs to be read from disk.
+
+        To be implemented in derived classes.
+        """
         raise NotImplementedError
 
 
@@ -147,6 +176,24 @@ class Speed(AudioTransform):
         augmented, new_sampling_rate = torchaudio.sox_effects.apply_effects_tensor(samples, sampling_rate, effect)
         return augmented.numpy()
 
+    def reverse_timestamps(
+            self,
+            offset: Seconds,
+            duration: Optional[Seconds],
+            sampling_rate: int
+    ) -> Tuple[Seconds, Optional[Seconds]]:
+        """
+        This method helps estimate the original offset and duration for a recording
+        before speed perturbation was applied.
+        We need this estimate to know how much audio to actually load from disk during the
+        call to ``load_audio()``.
+        """
+        start_sample = compute_num_samples(offset, sampling_rate)
+        num_samples = compute_num_samples(duration, sampling_rate) if duration is not None else None
+        start_sample = perturb_num_samples(start_sample, 1 / self.factor)
+        num_samples = perturb_num_samples(num_samples, 1 / self.factor) if num_samples is not None else None
+        return start_sample / sampling_rate, num_samples / sampling_rate if num_samples is not None else None
+
 
 @dataclass
 class Resample(AudioTransform):
@@ -162,6 +209,33 @@ class Resample(AudioTransform):
             samples = torch.from_numpy(samples)
         augmented, _ = torchaudio.sox_effects.apply_effects_tensor(samples, self.source_sampling_rate, effect)
         return augmented.numpy()
+
+    def reverse_timestamps(
+            self,
+            offset: Seconds,
+            duration: Optional[Seconds],
+            sampling_rate: int
+    ) -> Tuple[Seconds, Optional[Seconds]]:
+        """
+        This method helps estimate the original offset and duration for a recording
+        before resampling was applied.
+        We need this estimate to know how much audio to actually load from disk during the
+        call to ``load_audio()``.
+
+        In case of resampling, the timestamps might change slightly when using non-trivial
+        pairs of sampling rates, e.g. 16kHz -> 22.05kHz, because the number of samples in
+        the resampled audio might actually correspond to incrementally larger/smaller duration.
+        E.g. 16kHz, 235636 samples correspond to 14.72725s duration; after resampling to 22.05kHz,
+        it is 324736 samples which correspond to 14.727256235827664s duration.
+        """
+        old_num_samples = compute_num_samples(offset, self.source_sampling_rate, rounding=ROUND_HALF_UP)
+        old_offset = old_num_samples / self.source_sampling_rate
+        if duration is not None:
+            old_num_samples = compute_num_samples(duration, self.source_sampling_rate, rounding=ROUND_HALF_UP)
+            old_duration = old_num_samples / self.source_sampling_rate
+        else:
+            old_duration = None
+        return old_offset, old_duration
 
 
 def speed(sampling_rate: int) -> List[List[str]]:
