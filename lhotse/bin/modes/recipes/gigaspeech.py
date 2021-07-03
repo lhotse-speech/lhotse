68cddc3752c2ab4bfb92ea8c980447475734114a
Piotr Żelasko <petezor@gmail.com>
2021-07-02 21:38:23 -0400

    Add GigaSpeech CLI + fix the recipe

---
+++ lhotse/bin/modes/recipes/gigaspeech.py
@@ -0,0 +1,33 @@
+import click
+
+from lhotse.bin.modes import obtain, prepare
+from lhotse.recipes.gigaspeech import GIGASPEECH_PARTS, download_gigaspeech, prepare_gigaspeech
+from lhotse.utils import Pathlike
+
+
+@prepare.command(context_settings=dict(show_default=True))
+@click.argument('corpus_dir', type=click.Path(exists=True, dir_okay=True))
+@click.argument('output_dir', type=click.Path())
+@click.option('--subset', type=click.Choice(['auto'] + GIGASPEECH_PARTS),
+              default=True, help='Which parts of Gigaspeech to prepare (by default XL + DEV + TEST).')
+@click.option('-j', '--num-jobs', type=int, default=1,
+              help='How many threads to use (can give good speed-ups with slow disks).')
+def gigaspeech(
+        corpus_dir: Pathlike,
+        output_dir: Pathlike,
+        num_jobs: int
+):
+    """Gigaspeech ASR data preparation."""
+    prepare_gigaspeech(corpus_dir, output_dir=output_dir, num_jobs=num_jobs)
+
+
+@obtain.command(context_settings=dict(show_default=True))
+@click.argument('target_dir', type=click.Path())
+@click.option('--subset', type=click.Choice(['auto'] + GIGASPEECH_PARTS),
+              default=True, help='Which parts of Gigaspeech to download (by default XL + DEV + TEST).')
+def gigaspeech(
+        target_dir: Pathlike,
+        full: bool
+):
+    """Gigaspeech download."""
+    download_gigaspeech(target_dir, dataset_parts='librispeech' if full else 'mini_librispeech')
--- lhotse/recipes/gigaspeech.py
+++ lhotse/recipes/gigaspeech.py
@@ -1,3 +1,10 @@
+"""
+Description taken from the abstract of paper:
+"GigaSpeech: An Evolving, Multi-domain ASR Corpus with 10,000 Hours of Transcribed Audio"
+https://arxiv.org/abs/2106.06909
+
+This paper introduces GigaSpeech, an evolving, multi-domain English speech recognition corpus with 10,000 hours of high quality labeled audio suitable for supervised training, and 40,000 hours of total audio suitable for semi-supervised and unsupervised training. Around 40,000 hours of transcribed audio is first collected from audiobooks, podcasts and YouTube, covering both read and spontaneous speaking styles, and a variety of topics, such as arts, science, sports, etc. A new forced alignment and segmentation pipeline is proposed to create sentence segments suitable for speech recognition training, and to filter out segments with low-quality transcription. For system training, GigaSpeech provides five subsets of different sizes, 10h, 250h, 1000h, 2500h, and 10000h. For our 10,000-hour XL training subset, we cap the word error rate at 4% during the filtering/validation stage, and for all our other smaller training subsets, we cap it at 0%. The DEV and TEST evaluation sets, on the other hand, are re-processed by professional human transcribers to ensure high transcription quality. Baseline systems are provided for popular speech recognition toolkits, namely Athena, ESPnet, Kaldi and Pika.
+"""
 from collections import defaultdict
 from concurrent.futures.thread import ThreadPoolExecutor
 from pathlib import Path
@@ -10,9 +17,34 @@ from lhotse.recipes.utils import read_manifests_if_cached
 from lhotse.supervision import SupervisionSegment, SupervisionSet
 from lhotse.utils import Pathlike, Seconds, is_module_available
 
+GIGASPEECH_PARTS = ('{XL}', '{L}', '{M}', '{S}', '{XS}', '{DEV}', '{TEST}')
+
+
+def download_gigaspeech(
+        target_dir: Pathlike = '.',
+        dataset_parts: Optional[Union[str, Sequence[str]]] = "auto",
+):
+    if is_module_available('speechcolab'):
+        from speechcolab.datasets.gigaspeech import GigaSpeech
+    else:
+        raise ImportError(
+            'To process the GigaSpeech corpus, please install optional dependency: pip install speechcolab')
+    gigaspeech = GigaSpeech(target_dir)
+
+    if dataset_parts == 'auto':
+        dataset_parts = ('{XL}', '{DEV}', '{TEST}')
+    elif isinstance(dataset_parts, str):
+        dataset_parts = [dataset_parts]
+
+    for part in dataset_parts:
+        try:
+            gigaspeech.download(part)
+        except NotImplementedError:
+            raise ValueError(f"Could not download GigaSpeech part {part} -- speechcolab raised NotImplementedError.")
+
 
 def prepare_gigaspeech(
-        gigaspeech: Any,
+        corpus_dir: Pathlike,
         dataset_parts: Union[str, Sequence[str]] = 'auto',
         output_dir: Optional[Pathlike] = None,
         num_jobs: int = 1
@@ -24,6 +56,8 @@ def prepare_gigaspeech(
             'To process the GigaSpeech corpus, please install optional dependency: pip install speechcolab')
 
     subsets = ('{XL}', '{DEV}', '{TEST}') if dataset_parts == 'auto' else dataset_parts
+    corpus_dir = Path(corpus_dir)
+    gigaspeech = GigaSpeech(corpus_dir)
 
     if output_dir is not None:
         output_dir = Path(output_dir)
