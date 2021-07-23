9d631164305f5c64f86fda24dfedf6499c0d73c9
Piotr Żelasko <petezor@gmail.com>
2021-07-22 23:42:01 -0400

    Add some description

--- lhotse/dataset/dataloading.py
+++ lhotse/dataset/dataloading.py
@@ -1,48 +1,86 @@
+from collections import deque
 from concurrent.futures import ProcessPoolExecutor
+from typing import Any, Dict, List
+
+import torch.utils.data
+
+from lhotse.dataset.sampling import CutSampler
 
 
 class LhotseDataLoader:
-    def __init__(self, dataset, sampler, num_workers: int = 1, prefetch_factor: int = 2) -> None:
+    """
+    A simplified ``DataLoader`` implementation that relies on a ``ProcessPoolExecutor``.
+    The main difference between this and ``torch.utils.data.DataLoader`` is that
+    :class:`.LhotseDataLoader` allows to launch subprocesses inside of its workers.
+    This is useful for working with dataset classes which perform dynamic batching
+    and need to perform concurrent I/O to read all the necessary data from disk/network.
+    """
+    def __init__(
+        self,
+        dataset: torch.utils.data.Dataset,
+        sampler: CutSampler,
+        num_workers: int = 1,
+        prefetch_factor: int = 2,
+    ) -> None:
+        assert num_workers >= 1
+        assert prefetch_factor >= 1
         self.dataset = dataset
         self.sampler = sampler
-        self.pool = ProcessPoolExecutor(num_workers, initializer=_init_worker, initargs=(dataset,))
         self.num_workers = num_workers
         self.prefetch_factor = prefetch_factor
+        # Mutable state
         self._iter = None
-        self._futures = []
+        self._futures = deque([])
+        # Start the worker processes. The initializer receives the dataset object
+        # from the main process and caches it globally, so that it can be re-used
+        # for subsequent tasks sent to the worker. This helps avoid excessive
+        # communication between the processes.
+        self.pool = ProcessPoolExecutor(
+            num_workers, initializer=_init_worker, initargs=(dataset,)
+        )
 
-    def __iter__(self):
+    def __iter__(self) -> 'LhotseDataLoader':
+        """Prepares the sampler for iteration and schedules initial tasks to the workers."""
         self._iter = iter(self.sampler)
         for _ in range(self.prefetch_factor * self.num_workers):
             self._schedule_one()
         return self
 
-    def _schedule_one(self):
+    def _schedule_one(self) -> None:
+        """Submits a task and stores the future for results retrieval."""
         if self._iter is not None:
             try:
-                self._futures.append(
-                    self.pool.submit(_get_item, next(self._iter))
-                )
+                self._futures.append(self.pool.submit(_get_item, next(self._iter)))
             except StopIteration:
                 self._iter = None
 
-    def _retrieve_one(self):
+    def _retrieve_one(self) -> Dict[str, Any]:
+        """Retrieves the result from the earliest submitted task."""
         if self._futures:
-            return self._futures.pop().result()
+            return self._futures.popleft().result()
         raise StopIteration()
 
-    def __next__(self):
+    def __next__(self) -> Dict[str, Any]:
+        """Submits a new batch to process and then retrieves and returns a completed batch."""
         self._schedule_one()
         return self._retrieve_one()
 
 
-def _init_worker(dataset):
+def _init_worker(dataset: torch.utils.data.Dataset) -> None:
+    """
+    Stores the dataset in the global state of the process -- this is safe because
+    the process is initialized only once and used for unique dataset in its life span.
+    """
     global _GLOBAL_DATASET_CACHE
     _GLOBAL_DATASET_CACHE = dataset
 
 
-def _get_item(arg):
-    return _GLOBAL_DATASET_CACHE[arg]
+def _get_item(cut_ids: List[str]) -> Dict[str, Any]:
+    """
+    Queries the globally cached dataset to retrieve a batch. Has to be run
+    inside a worker process that was initialized with :meth:`._init_worker`.
+    """
+    return _GLOBAL_DATASET_CACHE[cut_ids]
 
 
-_GLOBAL_DATASET_CACHE = None
+_GLOBAL_DATASET_CACHE: torch.utils.data.Dataset = None
