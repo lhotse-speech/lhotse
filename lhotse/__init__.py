3719e1901a9bc5a95c799649f28bf1efed9fc02e
Piotr Żelasko <petezor@gmail.com>
2021-08-25 14:06:36 -0400

    Release v0.8 + add lhotse.__version__

--- lhotse/__init__.py
+++ lhotse/__init__.py
@@ -23,4 +23,23 @@ from . import features
 from . import recipes
 
 
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+# (this snippet is borrowed from scikit-learn's __init__.py)
+#
+# Generic release markers:
+#   X.Y.0   # For first release after an increment in Y
+#   X.Y.Z   # For bugfix releases
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+# >>>>>>>>>>>>>>>>>>> CAUTION <<<<<<<<<<<<<<<<<<<<<
+# > For public releases, remove the '.dev' suffix <
+# > For non-public releases, add a '.dev' suffix  <
+# >>>>>>>>>>>>>>>>>>> CAUTION <<<<<<<<<<<<<<<<<<<<<
+
+__version__ = "0.8.0"
+
+
 _add_tools_to_path()
--- setup.py
+++ setup.py
@@ -8,6 +8,62 @@ from setuptools import find_packages, setup
 
 project_root = Path(__file__).parent
 
+
+def discover_lhotse_version():
+    """
+    Scans Lhotse source code to determine the current version.
+    When development version is detected, it queries git for the commit hash
+    to append it as a local version identifier.
+    """
+
+    # Read the version from "lhotse.__version__"
+    try:
+        version_line = [
+            line
+            for line in (project_root / "lhotse" / "__init__.py")
+            .read_text()
+            .splitlines()
+            if line.startswith("__version__ = ")
+        ][0]
+        version = version_line.split('"')[1]
+    except IndexError:
+        print("Unable to discover Lhotse's version: specifying placeholder 0.0.0.dev0")
+        version = "0.0.0.dev0"
+
+    # .dev suffix is only going to be removed for public releases
+    if ".dev" not in version:
+        return version
+
+    # This is not a PyPI release -- try to read the git commit
+    try:
+        git_commit = (
+            run(["git", "rev-parse", "--short", "HEAD"], check=True, stdout=PIPE)
+            .stdout.decode()
+            .rstrip("\n")
+            .strip()
+        )
+        dirty_commit = (
+            len(
+                run(["git", "diff", "--shortstat"], check=True, stdout=PIPE)
+                .stdout.decode()
+                .rstrip("\n")
+                .strip()
+            )
+            > 0
+        )
+        git_commit = git_commit + ".dirty" if dirty_commit else git_commit + ".clean"
+    except Exception:
+        git_commit = ".unknowncommit"
+    # See the format:
+    # https://packaging.python.org/guides/distributing-packages-using-setuptools/#local-version-identifiers
+    version = version + '+git.' + git_commit
+
+    return version
+
+
+LHOTSE_VERSION = discover_lhotse_version()
+
+
 install_requires = [
     "audioread>=2.1.9",
     "SoundFile>=0.10",
@@ -50,45 +106,38 @@ try:
 except ImportError:
     install_requires.extend(["torch", "torchaudio"])
 
-docs_require = (project_root / 'docs' / 'requirements.txt').read_text().splitlines()
-tests_require = ['pytest==5.4.3', 'flake8==3.8.3', 'coverage==5.1', 'hypothesis==5.41.2']
-dev_requires = sorted(docs_require + tests_require + ['jupyterlab', 'matplotlib', 'isort'])
+docs_require = (project_root / "docs" / "requirements.txt").read_text().splitlines()
+tests_require = [
+    "pytest==5.4.3",
+    "flake8==3.8.3",
+    "coverage==5.1",
+    "hypothesis==5.41.2",
+]
+dev_requires = sorted(
+    docs_require + tests_require + ["jupyterlab", "matplotlib", "isort"]
+)
 all_requires = sorted(dev_requires)
 
-if os.environ.get('READTHEDOCS', False):
+if os.environ.get("READTHEDOCS", False):
     # When building documentation, omit torchaudio installation and mock it instead.
     # This works around the inability to install libsoundfile1 in read-the-docs env,
     # which caused the documentation builds to silently crash.
     install_requires = [
-        req for req in install_requires
-        if not any(
-            req.startswith(dep) for dep in ['torchaudio', 'SoundFile']
-        )]
-
-try:
-    git_commit = run(['git', 'rev-parse', '--short', 'HEAD'], check=True, stdout=PIPE).stdout.decode().rstrip(
-        '\n').strip()
-    dirty_commit = len(
-        run(['git', 'diff', '--shortstat'], check=True, stdout=PIPE).stdout.decode().rstrip('\n').strip()) > 0
-    git_commit = git_commit + '-dirty' if dirty_commit else git_commit + '-clean'
-except Exception:
-    git_commit = ''
-# See the format https://packaging.python.org/guides/distributing-packages-using-setuptools/#local-version-identifiers
-dev_version = '.dev-' + git_commit
-
-if os.environ.get('RELEASE', False):
-    dev_version = ''
+        req
+        for req in install_requires
+        if not any(req.startswith(dep) for dep in ["torchaudio", "SoundFile"])
+    ]
 
 setup(
-    name='lhotse',
-    version='0.8.0' + dev_version,
-    python_requires='>=3.6.0',
-    description='Data preparation for speech processing models training.',
-    author='The Lhotse Development Team',
+    name="lhotse",
+    version=LHOTSE_VERSION,
+    python_requires=">=3.6.0",
+    description="Data preparation for speech processing models training.",
+    author="The Lhotse Development Team",
     author_email="pzelasko@jhu.edu",
-    long_description=(project_root / 'README.md').read_text(encoding='utf-8'),
+    long_description=(project_root / "README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
-    license='Apache-2.0 License',
+    license="Apache-2.0 License",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
@@ -97,10 +146,10 @@ setup(
     },
     install_requires=install_requires,
     extras_require={
-        'docs': docs_require,
-        'tests': tests_require,
-        'dev': dev_requires,
-        'all': all_requires
+        "docs": docs_require,
+        "tests": tests_require,
+        "dev": dev_requires,
+        "all": all_requires,
     },
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -114,6 +163,6 @@ setup(
         "Topic :: Multimedia :: Sound/Audio :: Speech",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Typing :: Typed"
+        "Typing :: Typed",
     ],
 )
