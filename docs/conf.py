b3c4db1cd7e22ee4dbfd8a5c3bfca6851605c76a
Piotr Żelasko <petezor@gmail.com>
2021-10-12 16:59:45 -0400

    Reformat everything with black

--- docs/conf.py
+++ docs/conf.py
@@ -19,12 +19,12 @@ import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
-project = 'lhotse'
-copyright = '2020-2021, Lhotse development team'
-author = 'Lhotse development team'
+project = "lhotse"
+copyright = "2020-2021, Lhotse development team"
+author = "Lhotse development team"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 
@@ -32,20 +32,20 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx_rtd_theme',
-    'sphinx_click.ext',
-    'sphinx_autodoc_typehints',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx_rtd_theme",
+    "sphinx_click.ext",
+    "sphinx_autodoc_typehints",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -53,21 +53,21 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 autodoc_default_options = {
-    'members': True,
-    'member-order': 'bysource',
-    'special-members': '__init__',
-    'undoc-members': True,
-    'exclude-members': '__weakref__'
+    "members": True,
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+    "exclude-members": "__weakref__",
 }
 
 autodoc_mock_imports = ["torchaudio", "SoundFile"]
--- lhotse/__init__.py
+++ lhotse/__init__.py
@@ -1,13 +1,13 @@
 from .audio import AudioSource, Recording, RecordingSet
 from .augmentation import *
-from .cut import MonoCut, CutSet
+from .cut import CutSet, MonoCut
 from .features import *
 from .kaldi import load_kaldi_data_dir
-from .manipulation import combine, to_manifest, split_parallelize_combine
+from .manipulation import combine, split_parallelize_combine, to_manifest
+from .qa import fix_manifests, validate, validate_recordings_and_supervisions
 from .serialization import load_manifest, store_manifest
 from .supervision import SupervisionSegment, SupervisionSet
 from .tools.env import add_tools_to_path as _add_tools_to_path
-from .qa import validate, validate_recordings_and_supervisions, fix_manifests
 
 try:
     # Try to get Lhotse's version (should be created during running pip install / python setup.py ...)
@@ -17,7 +17,7 @@ except:
     # NOTE: REMEMBER TO UPDATE THE ACTUAL VERSION IN setup.py WHEN RELEASING #
     # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #
     # Use a default placeholder when the version is unavailable...
-    __version__ = '0.10.0.dev+missing.version.file'
+    __version__ = "0.10.0.dev+missing.version.file"
 
 from . import augmentation
 from . import dataset
