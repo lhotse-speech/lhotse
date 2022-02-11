d5de0b470c86ebf372123808e773369d21b7891d
Piotr Żelasko <petezor@gmail.com>
2022-02-11 18:59:42 -0500

    Remove redundant code, fix tests

--- test/cut/test_cut_with_in_memory_audio.py
+++ test/cut/test_cut_with_in_memory_audio.py
@@ -4,24 +4,7 @@ from lhotse import Recording, MonoCut
 from lhotse.testing.dummies import dummy_cut
 
 
-def test_cut_with_in_memory_audio():
-    path = "test/fixtures/mono_c0.wav"
-    data = open(path, "rb").read()
-    cut = dummy_cut(0, duration=0.5).drop_recording()
-
-    assert cut.custom is None
-    assert cut.load_audio() is None  # original cut has no audio data
-
-    cut_with_audio = cut.with_memory_audio(data)
-
-    assert cut.custom is None  # original cut is unmodified
-
-    np.testing.assert_equal(
-        cut_with_audio.load_audio(), Recording.from_file(path).load_audio()
-    )
-
-
-def test_cut_to_in_memory():
+def test_cut_move_to_memory():
     path = "test/fixtures/mono_c0.wav"
     cut = dummy_cut(0, duration=0.5).drop_recording()
     cut.recording = Recording.from_file(path)
@@ -33,15 +16,12 @@ def test_cut_to_in_memory():
     )
 
 
-def test_cut_with_in_memory_audio_serialization():
+def test_cut_move_to_memory_audio_serialization():
     path = "test/fixtures/mono_c0.wav"
-    data = open(path, "rb").read()
     cut = dummy_cut(0, duration=0.5).drop_recording()
+    cut.recording = Recording.from_file(path)
 
-    assert cut.custom is None
-    assert cut.load_audio() is None  # original cut has no audio data
-
-    cut_with_audio = cut.with_memory_audio(data)
+    cut_with_audio = cut.move_to_memory()
 
     assert cut.custom is None  # original cut is unmodified
 
