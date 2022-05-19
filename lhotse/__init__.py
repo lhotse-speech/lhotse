024890f960f0ee4036ef1b636b9e239cecf88e4a
Piotr Żelasko <petezor@gmail.com>
2022-05-19 12:57:41 -0400

    Release v1.2

--- lhotse/__init__.py
+++ lhotse/__init__.py
@@ -33,12 +33,8 @@ except:
     # NOTE: REMEMBER TO UPDATE THE ACTUAL VERSION IN setup.py WHEN RELEASING #
     # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #
     # Use a default placeholder when the version is unavailable...
-    __version__ = "1.2.0.dev+missing.version.file"
+    __version__ = "1.2.0+missing.version.file"
 
-from . import augmentation
-from . import dataset
-from . import features
-from . import recipes
-
+from . import augmentation, dataset, features, recipes
 
 _add_tools_to_path()
