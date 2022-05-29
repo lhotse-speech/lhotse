51922d1aa1e43a3b1e1f57dd5e9d3db048d814a8
Amir Hussein <36240131+AmirHussein96@users.noreply.github.com>
2022-05-28 20:42:43 -0400

    Update mgb2.py
    Fixing unicode string

--- lhotse/recipes/mgb2.py
+++ lhotse/recipes/mgb2.py
@@ -183,16 +183,18 @@ def prepare_mgb2(
     return manifests
 
 
-_unicode = "\u0622\u0624\u0626\u0628\u062a\u062c\u06af\u062e\u0630\u0632\u0634\
-    \u0636\u0638\u063a\u0640\u0642\u0644\u0646\u0648\u064a\u064c\u064e\u0650\u0652\u0670\
-    \u067e\u0686\u0621\u0623\u0625\u06a4\u0627\u0629\u062b\u062d\u062f\u0631\u0633\u0635\
-    \u0637\u0639\u0641\u0643\u0645\u0647\u0649\u064b\u064d\u064f\u0651\u0671"
-_buckwalter = "|&}btjGx*z$DZg_qlnwyNaio`PJ'><VApvHdrsSTEfkmhYFKu~{"
-_backwardMap = {ord(a): b for a, b in zip(_buckwalter, _unicode)}
+_unicode = u"\u0622\u0624\u0626\u0628\u062a\u062c\u06af\u062e\u0630\u0632" \
+    "\u0634\u0636\u0638\u063a\u0640\u0642\u0644\u0646\u0648\u064a\u064c\u064e" \
+    "\u0650\u0652\u0670\u067e\u0686\u0621\u0623\u0625\u06a4\u0627\u0629\u062b" \
+    "\u062d\u062f\u0631\u0633\u0635\u0637\u0639\u0641\u0643\u0645\u0647\u0649" \
+    "\u064b\u064d\u064f\u0651\u0671"
+_buckwalter = u"|&}btjGx*z$DZg_qlnwyNaio`PJ'><VApvHdrsSTEfkmhYFKu~{"
+
+_backward_map = {ord(b): a for a, b in zip(_unicode, _buckwalter)}
 
 
 def from_buck_walter(s: str) -> str:
-    return s.translate(_backwardMap)
+    return s.translate(_backward_map)
 
 
 def remove_diacritics(text: str) -> str:
