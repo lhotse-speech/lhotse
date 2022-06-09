65c5f36c43737a6c2f08cb0ecc5fbeef31664941
luomingshuang <739314837@qq.com>
2022-06-09 20:49:51 +0800

    do a change

--- lhotse/recipes/tal_csasr.py
+++ lhotse/recipes/tal_csasr.py
@@ -47,59 +47,56 @@ def prepare_tal_csasr(
     manifests = defaultdict(dict)
     dataset_parts = ["train_set", "dev_set", "test_set"]
 
-    with ThreadPoolExecutor(num_jobs) as ex:
-        # for part in tqdm(dataset_parts, desc="Dataset part"):
-        for part in dataset_parts:
-            logging.info(f"Prepare manifest for {part}")
-            transcript_dict = {}
-            with open(corpus_dir / f"{part}/label.txt", "r", encoding="utf-8") as f:
-                for line in f.readlines():
-                    idx_transcript = line.rstrip("\n").split(" ")
-                    transcript_dict[idx_transcript[0]] = " ".join(idx_transcript[1:])
+    # for part in tqdm(dataset_parts, desc="Dataset part"):
+    for part in dataset_parts:
+        logging.info(f"Prepare manifest for {part}")
+        transcript_dict = {}
+        with open(corpus_dir / f"{part}/label.txt", "r", encoding="utf-8") as f:
+            for line in f:
+                idx_transcript = line.split()
+                transcript_dict[idx_transcript[0]] = " ".join(idx_transcript[1:])
 
-            recordings = []
-            supervisions = []
-            wav_path = corpus_dir / f"{part}" / "wav"
-            wav_files = glob.glob(os.path.join(wav_path, "*.wav"))
-            for i in tqdm(range(len(wav_files))):
-                audio_path = wav_files[i]
-                idx = Path(audio_path).stem
-                if idx not in transcript_dict:
-                    logging.warning(f"No transcript: {idx}")
-                    continue
-                text = transcript_dict[idx]
-                if not Path(audio_path).is_file():
-                    logging.warning(f"No such file: {audio_path}")
-                    continue
-                recording = Recording.from_file(path=audio_path, recording_id=idx)
-                recordings.append(recording)
-                segment = SupervisionSegment(
-                    id=idx,
-                    recording_id=idx,
-                    start=0.0,
-                    duration=recording.duration,
-                    channel=0,
-                    language="English-Chinese",
-                    text=text.strip(),
-                )
+        recordings = []
+        supervisions = []
+        wav_path = corpus_dir / f"{part}" / "wav"
+        wav_files = glob.glob(os.path.join(wav_path, "*.wav"))
+        for i in tqdm(range(len(wav_files))):
+            audio_path = wav_files[i]
+            idx = Path(audio_path).stem
+            if idx not in transcript_dict:
+                logging.warning(f"No transcript: {idx}")
+                continue
+            text = transcript_dict[idx]
+            if not Path(audio_path).is_file():
+                logging.warning(f"No such file: {audio_path}")
+                continue
+            recording = Recording.from_file(path=audio_path, recording_id=idx)
+            recordings.append(recording)
+            segment = SupervisionSegment(
+                id=idx,
+                recording_id=idx,
+                start=0.0,
+                duration=recording.duration,
+                channel=0,
+                language="English-Chinese",
+                text=text.strip(),
+            )
 
-                supervisions.append(segment)
+            supervisions.append(segment)
 
-                recording_set = RecordingSet.from_recordings(recordings)
-                supervision_set = SupervisionSet.from_segments(supervisions)
-                validate_recordings_and_supervisions(recording_set, supervision_set)
+        recording_set = RecordingSet.from_recordings(recordings)
+        supervision_set = SupervisionSet.from_segments(supervisions)
+        validate_recordings_and_supervisions(recording_set, supervision_set)
 
-                if output_dir is not None:
-                    supervision_set.to_file(
-                        output_dir / f"tal_csasr_supervisions_{part}.jsonl.gz"
-                    )
-                    recording_set.to_file(
-                        output_dir / f"tal_csasr_recordings_{part}.jsonl.gz"
-                    )
+        if output_dir is not None:
+            supervision_set.to_file(
+                output_dir / f"tal_csasr_supervisions_{part}.jsonl.gz"
+            )
+            recording_set.to_file(output_dir / f"tal_csasr_recordings_{part}.jsonl.gz")
 
-                manifests[part] = {
-                    "recordings": recording_set,
-                    "supervisions": supervision_set,
-                }
+        manifests[part] = {
+            "recordings": recording_set,
+            "supervisions": supervision_set,
+        }
 
     return manifests
