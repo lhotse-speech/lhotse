fc962e10f302decb9fedaa6fb8339888d766547b
Piotr Żelasko <petezor@gmail.com>
2022-07-05 14:14:32 -0400

    Data preparation for DailyTalk

--- lhotse/bin/modes/recipes/daily_talk.py
+++ lhotse/bin/modes/recipes/daily_talk.py
@@ -8,16 +8,17 @@ from lhotse.utils import Pathlike
 @prepare.command()
 @click.argument("corpus_dir", type=click.Path(exists=True, dir_okay=True))
 @click.argument("output_dir", type=click.Path())
-def daily_talk(corpus_dir: Pathlike, output_dir: Pathlike):
+@click.option("--num-jobs", type=int, default=1, help="Number of parallel workers.")
+def daily_talk(corpus_dir: Pathlike, output_dir: Pathlike, num_jobs: int):
     """
     DailyTalk recording and supervision manifest preparation.
     """
-    prepare_daily_talk(corpus_dir, output_dir)
+    prepare_daily_talk(corpus_dir, output_dir, num_jobs=num_jobs)
 
 
 @download.command()
-@click.argument("target_dir", type=click.Path(exists=True, dir_okay=True))
-@click.option("--force-download", is_flag=True, help="Force download")
+@click.argument("target_dir", type=click.Path())
+@click.option("--force-download", is_flag=True, help="Force download.")
 def daily_talk(target_dir: Pathlike, force_download: bool = False):
     """
     Download DailyTalk dataset.
--- lhotse/recipes/daily_talk.py
+++ lhotse/recipes/daily_talk.py
@@ -7,9 +7,8 @@ Paper: https://arxiv.org/abs/2207.01063
 GitHub: https://github.com/keonlee9420/DailyTalk
 """
 import logging
-import subprocess
 from pathlib import Path
-from typing import Dict, Union
+from typing import Tuple
 
 from lhotse import (
     RecordingSet,
@@ -17,10 +16,12 @@ from lhotse import (
     SupervisionSet,
     validate_recordings_and_supervisions,
 )
-from lhotse.audio import Recording
-from lhotse.utils import Pathlike
+from lhotse.serialization import load_json
+from lhotse.utils import Pathlike, is_module_available
 
-DAILY_TALK_URL = "https://drive.google.com/file/d/1nPrfJn3TcIVPc0Uf5tiAXUYLJceb_5k-"
+DAILY_TALK_URL = (
+    "https://drive.google.com/file/d/1nPrfJn3TcIVPc0Uf5tiAXUYLJceb_5k-/view?usp=sharing"
+)
 
 
 def download_daily_talk(target_dir: Pathlike, force_download: bool = False) -> Path:
@@ -30,17 +31,21 @@ def download_daily_talk(target_dir: Pathlike, force_download: bool = False) -> P
     :param force_download: if True, it will download the DailyTalk data even if it is already present.
     :return: the path to downloaded and extracted directory with data.
     """
-    command = f"""wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate '{DAILY_TALK_URL}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1nPrfJn3TcIVPc0Uf5tiAXUYLJceb_5k-" -O daily_talk.zip && rm -rf /tmp/cookies.txt"""
+    if not is_module_available("gdown"):
+        raise ValueError("Please run 'pip install gdown' to download DailyTalk.")
+
+    import gdown
 
     target_dir = Path(target_dir)
     target_dir.mkdir(parents=True, exist_ok=True)
-    corpus_dir = target_dir / "daily_talk"
+    corpus_dir = target_dir / "dailytalk"
     corpus_zip = corpus_dir.with_suffix(".zip")
 
     if not force_download and corpus_zip.exists():
         logging.info(f"{corpus_zip} already exists. Skipping download.")
     else:
-        subprocess.run(command, shell=True, cwd=target_dir)
+        logging.info(f"Running: gdown --fuzzy {DAILY_TALK_URL}")
+        gdown.download(DAILY_TALK_URL, str(corpus_zip), quiet=False)
 
     # Extract the zipped file
     if not corpus_dir.exists() or force_download:
@@ -53,20 +58,62 @@ def download_daily_talk(target_dir: Pathlike, force_download: bool = False) -> P
 def prepare_daily_talk(
     corpus_dir: Pathlike,
     output_dir: Pathlike = None,
-) -> Dict[str, Dict[str, Union[RecordingSet, SupervisionSet]]]:
+    num_jobs: int = 1,
+) -> Tuple[RecordingSet, SupervisionSet]:
     """
-    Returns the manifests which consist of the Recordings and Supervisions.
-    When all the manifests are available in the ``output_dir``, it will simply read and return them.
-
-    NOTE: The recordings contain all 7 channels. If you want to use only one channel, you can
-    use either ``recording.load_audio(channel=0)`` or ``MonoCut(id=...,recording=recording,channel=0)``
-    while creating the CutSet.
+    Create RecordingSet and SupervisionSet manifests for DailyTalk from a raw corpus distribution.
 
     :param corpus_dir: Pathlike, the path to the extracted corpus.
     :param output_dir: Pathlike, the path where to write the manifests.
-    :param type: str, the type of data to prepare ('mdm', 'sdm', 'ihm-mix', or 'ihm'). These settings
-        are similar to the ones in AMI and ICSI recipes.
     :return: a Dict whose key is the dataset part, and the value is Dicts with the keys 'audio' and 'supervisions'.
-
     """
-    raise NotImplementedError()
+    corpus_dir = Path(corpus_dir)
+    meta = corpus_dir / "metadata.json"
+    data = corpus_dir / "data"
+
+    recordings = RecordingSet.from_dir(
+        data, "*.wav", num_jobs=num_jobs, recording_id=make_recording_id
+    )
+    supervisions = []
+    for dialog_idx, dialog in load_json(meta).items():
+        for utt_idx, utt in dialog.items():
+            recording_id = f"dailytalk_{utt_idx}_{utt['speaker']}_d{dialog_idx}"
+            assert utt["speaker"] in [
+                0,
+                1,
+            ], f"Unknown speaker index: {utt['speaker']}"
+            supervisions.append(
+                SupervisionSegment(
+                    id=f"dailytalk_{utt['index']}",
+                    recording_id=recording_id,
+                    start=0.0,
+                    duration=recordings[recording_id].duration,
+                    channel=0,
+                    text=utt["text"],
+                    language="English",
+                    speaker=f"dailytalk_spk{utt['speaker']}",
+                    gender="F" if utt["speaker"] == 1 else "M",
+                    custom={
+                        "turn": utt["turn"],
+                        "topic": utt["topic"],
+                        "emotion": utt["emotion"],
+                        "act": utt["act"],
+                    },
+                )
+            )
+    supervisions = SupervisionSet.from_segments(supervisions)
+    validate_recordings_and_supervisions(
+        recordings=recordings, supervisions=supervisions
+    )
+
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        output_dir.mkdir(exist_ok=True, parents=True)
+        recordings.to_file(output_dir / "dailytalk_recordings_all.jsonl.gz")
+        supervisions.to_file(output_dir / "dailytalk_supervisions_all.jsonl.gz")
+
+    return recordings, supervisions
+
+
+def make_recording_id(path: Path) -> str:
+    return f"dailytalk_{path.stem}"
