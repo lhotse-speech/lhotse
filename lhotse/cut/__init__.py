f17688d08f1a5556310d23400cfed6261b9b35a7
Desh Raj <r.desh26@gmail.com>
2022-09-24 17:08:44 -0400

    add init file

---
+++ lhotse/cut/__init__.py
@@ -0,0 +1,48 @@
+from .base import Cut
+from .mono import MonoCut
+from .mixed import MixedCut, MixTrack
+from .padding import PaddingCut
+from .set import CutSet
+
+"""
+The following is the hierarchy of imports in this module (to avoid circular imports):
+      ┌─────────────┐
+      │ __init__.py │─────────────────────────────────┬────────────────────┐
+      └─────────────┘                                 │                    │
+             │                                        ▼                    │
+             │                               ┌────────────────┐            │
+             ├──────────────────────┬───────▶│  mono.MonoCut  │────────────┤
+             │                      │        └────────────────┘            │
+             │                      │                                      │
+             │                      │                                      │
+             │           ┌────────────────────┐                            ▼
+             │           │   mixed.MixTrack   │                     ┌─────────────┐
+             ├──────────▶│   mixed.MixedCut   │────────────────────▶│  base.Cut   │
+             │           └────────────────────┘                     └─────────────┘
+             │                      │                                      ▲
+             │                      │                                      │
+             │                      │                                      │
+             │                      │        ┌────────────────────┐        │
+             ├──────────────────────┴───────▶│ padding.PaddingCut │────────┤
+             │                               └────────────────────┘        │
+    ┌────────────────┐                                  ▲                  │
+    │   set.CutSet   │──────────────────────────────────┴──────────────────┘
+    └────────────────┘
+"""
+
+# The following functions are imported in other modules, so we need to import them here.
+__all__ = [
+    "create_cut_set_eager",
+    "create_cut_set_lazy",
+    "compute_supervisions_frame_mask",
+    "append_cuts",
+    "mix_cuts",
+]
+
+from .set import (
+    create_cut_set_eager,
+    create_cut_set_lazy,
+    compute_supervisions_frame_mask,
+    append_cuts,
+    mix_cuts,
+)
