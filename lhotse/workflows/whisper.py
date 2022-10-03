0d7fa2cedb5dad78a6902c0e1431192a47d20759
Desh Raj <r.desh26@gmail.com>
2022-10-03 09:38:20 -0400

    use cutset for whisper workflow

--- lhotse/workflows/whisper.py
+++ lhotse/workflows/whisper.py
@@ -1,29 +1,30 @@
 import logging
-from typing import Generator, List, Optional
+from typing import Generator, List, Optional, Union
 
 import torch
 
-from lhotse import MonoCut, RecordingSet, SupervisionSegment
+from lhotse import CutSet, MonoCut, Recording, RecordingSet, SupervisionSegment
 from lhotse.qa import trim_supervisions_to_recordings
 from lhotse.utils import is_module_available
 
 
 def annotate_with_whisper(
-    recordings: RecordingSet,
+    manifest: Union[RecordingSet, CutSet],
     language: Optional[str] = None,
     model_name: str = "base",
     device: str = "cpu",
 ) -> Generator[MonoCut, None, None]:
     """
-    Use OpenAI Whisper model to annotate either RECORDINGS_MANIFEST or RECORDINGS_DIR.
-    It will perform automatic segmentation, transcription, and language identification.
+    Use OpenAI Whisper model to annotate either RECORDINGS_MANIFEST, RECORDINGS_DIR, or CUTS_MANIFEST.
+    It will perform automatic segmentation, transcription, and language identification. If
+    the first argument is a CutSet, it will overwrite the supervisions with the results of the inference.
 
     Note: this is an experimental feature of Lhotse, and is not guaranteed to yield
     high quality of data.
 
     See the original repo for more details: https://github.com/openai/whisper
 
-    :param recordings: a ``RecordingSet`` object.
+    :param manifest: a ``RecordingSet`` or ``CutSet`` object.
     :param language: specify the language if known upfront, otherwise it will be auto-detected.
     :param model_name: one of available Whisper variants (base, medium, large, etc.).
     :param device: Where to run the inference (cpu, cuda, etc.).
@@ -39,19 +40,21 @@ def annotate_with_whisper(
 
     model = whisper.load_model(model_name, device=device)
 
-    for recording in recordings:
-        if recording.num_channels > 1:
+    for item in manifest:
+        if item.num_channels > 1:
             logging.warning(
-                f"Skipping recording '{recording.id}'. It has {recording.num_channels} channels, "
+                f"Skipping recording or cut '{item.id}'. It has {item.num_channels} channels, "
                 f"but we currently only support mono input."
             )
             continue
-        audio = torch.from_numpy(recording.resample(16000).load_audio()).squeeze(0)
+        audio = torch.from_numpy(item.resample(16000).load_audio()).squeeze(0)
         result = whisper.transcribe(model=model, audio=audio, language=language)
         supervisions = [
             SupervisionSegment(
-                id=f"{recording.id}-{segment['id']:06d}",
-                recording_id=recording.id,
+                id=f"{item.id}-{segment['id']:06d}",
+                recording_id=item.id
+                if isinstance(item, Recording)
+                else item.recording_id,
                 start=round(segment["start"], ndigits=8),
                 duration=round(segment["end"], ndigits=8),
                 text=segment["text"].strip(),
@@ -59,12 +62,13 @@ def annotate_with_whisper(
             )
             for segment in result["segments"]
         ]
-        cut = recording.to_cut()
+        cut = item.to_cut() if isinstance(item, Recording) else item
+        recording = item if isinstance(item, Recording) else item.recording
         if supervisions:
             supervisions = _postprocess_timestamps(supervisions)
             cut.supervisions = list(
                 trim_supervisions_to_recordings(
-                    recordings=recordings, supervisions=supervisions, verbose=False
+                    recordings=recording, supervisions=supervisions, verbose=False
                 )
             )
         yield cut
