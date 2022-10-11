a5cf356583e4e723bc51549019599ad35d9666c0
Desh Raj <r.desh26@gmail.com>
2022-10-11 13:34:46 -0400

    Update supervisions channels in multi-channel recipes (#838)
    * update channel attribute of supervisions
    
    * fix in libricss supervision channels and qa
    
    * fix isort

--- lhotse/recipes/aspire.py
+++ lhotse/recipes/aspire.py
@@ -158,6 +158,9 @@ def prepare_aspire(
                     speaker=speaker,
                     text=seg.text,
                     language="English",
+                    channel=0
+                    if mic == "single"
+                    else recording_set[session].channel_ids,
                 )
                 for i, seg in enumerate(segs)
             ]
