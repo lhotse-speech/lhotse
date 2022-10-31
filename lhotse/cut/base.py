67a8830450ee5e57f2d42bafed29d6d2d63fee18
Desh Raj <r.desh26@gmail.com>
2022-10-31 09:29:16 -0400

    add option for separate alignment directory

--- lhotse/cut/base.py
+++ lhotse/cut/base.py
@@ -486,9 +486,9 @@ class Cut:
 
                 # Ensure that all supervisions have the same channel.
                 assert len(set(s.channel for s in trimmed.supervisions)) == 1, (
-                    "Trimmed cut has supervisions with different channels. Either set "
-                    "`ignore_channel=True` to keep original channels or `keep_overlapping=False` "
-                    "to retain only 1 supervision per trimmed cut."
+                    f"Trimmed cut has supervisions with different channels. Either set "
+                    f"`ignore_channel=True` to keep original channels or `keep_overlapping=False` "
+                    f"to retain only 1 supervision per trimmed cut. Offending cut: {trimmed}"
                 )
                 trimmed.channel = trimmed.supervisions[0].channel
 
