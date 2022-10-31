8b7e4488f9c1d9cce80b71da32ad874c2ad18fe2
Desh Raj <r.desh26@gmail.com>
2022-10-31 09:30:00 -0400

    AR-RNNT dataset

--- lhotse/dataset/__init__.py
+++ lhotse/dataset/__init__.py
@@ -11,7 +11,10 @@ from .source_separation import (
     PreMixedSourceSeparationDataset,
     SourceSeparationDataset,
 )
-from .speech_recognition import K2SpeechRecognitionDataset
+from .speech_recognition import (
+    K2AlignmentRestrictedTrasducerDataset,
+    K2SpeechRecognitionDataset,
+)
 from .speech_synthesis import SpeechSynthesisDataset
 from .unsupervised import (
     DynamicUnsupervisedDataset,
