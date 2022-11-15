dbc50312e4dc989b6fad80260c8686d303dc5830
Desh Raj <r.desh26@gmail.com>
2022-11-15 10:01:13 -0500

    more progress on simulators

--- lhotse/workflows/__init__.py
+++ lhotse/workflows/__init__.py
@@ -1,3 +1,6 @@
 from .forced_alignment import align_with_torchaudio
-from .meeting_simulation import SpeakerIndependentMeetingSimulator
+from .meeting_simulation import (
+    ConversationalMeetingSimulator,
+    SpeakerIndependentMeetingSimulator,
+)
 from .whisper import annotate_with_whisper
