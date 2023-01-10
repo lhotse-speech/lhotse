e9135f85111140b7e813cbe360329fe4b420cd11
Teo <teouuenshen@gmail.com>
2023-01-10 15:51:51 +0900

    Parse sdb faithfully

--- lhotse/bin/modes/recipes/csj.py
+++ lhotse/bin/modes/recipes/csj.py
@@ -14,21 +14,6 @@ __all__ = ["csj"]
 @click.argument("transcript_dir", type=click.Path())
 @click.argument("manifest_dir", type=click.Path())
 @click.option(
-    "-c",
-    "--configs",
-    type=click.Path(exists=True, file_okay=True),
-    default=None,
-    multiple=True,
-    help=(
-        "List of .ini config files to use when parsing transcripts. "
-        "Pass each with `-c`. Example: "
-        "`-c local/config/disfluent.ini -c local/config/fluent.ini` "
-        "NOTE: Only the segment specifications in the first config file are "
-        "used. The remaining config files that come after will inherit "
-        "the segment boundaries of the first config file. "
-    ),
-)
-@click.option(
     "-p",
     "--dataset-parts",
     type=str,
@@ -51,7 +36,6 @@ def csj(
     corpus_dir: Pathlike,
     transcript_dir: Pathlike,
     manifest_dir: Pathlike,
-    configs: Union[Pathlike, Sequence[Pathlike]],
     dataset_parts: Union[str, Sequence[str]],
     num_jobs: int,
 ):
@@ -62,6 +46,5 @@ def csj(
         transcript_dir=transcript_dir,
         manifest_dir=manifest_dir,
         dataset_parts=dataset_parts,
-        configs=configs,
         nj=num_jobs,
     )
