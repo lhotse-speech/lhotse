146db169f81ed0ad3d24649f51deb6539b712b3f
Piotr Żelasko <petezor@gmail.com>
2023-02-21 09:54:55 -0500

    Add RecordingChunkIterableDataset

--- lhotse/dataset/unsupervised.py
+++ lhotse/dataset/unsupervised.py
@@ -1,10 +1,12 @@
+import math
 import warnings
-from typing import Any, Dict, Optional
+from typing import Any, Dict, List, Optional
 
 import torch
+from torch.utils.data import IterableDataset, default_collate
 
-from lhotse import validate
-from lhotse.audio import suppress_audio_loading_errors
+from lhotse import RecordingSet, Seconds, compute_num_samples, validate
+from lhotse.audio import suppress_audio_loading_errors, torchaudio_supports_ffmpeg
 from lhotse.augmentation import AugmentFn
 from lhotse.cut import CutSet
 from lhotse.dataset.collation import collate_audio, collate_features, collate_matrices
@@ -118,3 +120,117 @@ class DynamicUnsupervisedDataset(UnsupervisedDataset):
     def _validate(self, cuts: CutSet) -> None:
         validate(cuts)
         assert all(cut.has_recording for cut in cuts)
+
+
+class RecordingChunkIterableDataset(IterableDataset):
+    """
+    This dataset iterates over chunks of a recording, for each recording provided.
+
+    The format of yielded items is the following::
+        {
+            "recording_id": str
+            "begin_time": tensor with dtype=float32 shape=(1,)
+            "end_time": tensor with dtype=float32 shape=(1,)
+            "audio": tensor with dtype=float32 shape=(chunk_size_in_samples,)
+        }
+
+    Unlike most other datasets in Lhotse, this dataset does not yield batched items,
+    and should be used like the following::
+
+        >>> recordings = RecordingSet.from_file("my-recordings.jsonl.gz")
+        ... dataset = RecordingChunkIterableDataset(recordings, chunk_size=30.0, chunk_shift=25.0)
+        ... dloader = torch.utils.data.DataLoader(
+        ...     dataset,
+        ...     batch_size=32,
+        ...     collate_fn=audio_chunk_collate,
+        ...     worker_init_fn=audio_chunk_worker_init_fn,
+        ... )
+
+    """
+
+    def __init__(
+        self, recordings: RecordingSet, chunk_size: Seconds, chunk_shift: Seconds
+    ) -> None:
+        self.recordings = list(recordings)
+        self.chunk_size = chunk_size
+        self.chunk_shift = chunk_shift
+        self.start = 0
+        self.end = len(self.recordings)
+
+        self.validate()
+
+    def validate(self) -> None:
+
+        assert (
+            self.chunk_shift == self.chunk_size
+        ), "We currently don't support chunk shift != chunk_size (yet)."
+
+        if not torchaudio_supports_ffmpeg():
+            raise RuntimeError(
+                "Using FFMPEG streamer backend for reading is supported only "
+                "with PyTorch 1.12+ and torchaudio 0.12+"
+            )
+
+        for r in self.recordings:
+            assert (
+                len(r.sources) == 1
+            ), f"We currently don't support multi-source audio in this dataset (got {len(r.sources)} sources in recording {r.id})."
+            assert (
+                r.sources[0].type == "file"
+            ), f"We currently only support 'file' AudioSource type in this dataset (got: {r.sources[0].type} in recording {r.id})."
+            assert (
+                r.num_channels == 1
+            ), f"We currently only support single-channel audio in this dataset (got {r.num_channels} channels in recording {r.id})."
+
+    def __iter__(self):
+        import torchaudio
+
+        for r in self.recordings[self.start : self.end]:
+            chunk_size = compute_num_samples(self.chunk_size, r.sampling_rate)
+            chunk_shift = compute_num_samples(self.chunk_shift, r.sampling_rate)
+
+            streamer = torchaudio.io.StreamReader(src=r.sources[0].source)
+            assert streamer.num_src_streams == 1, (
+                "Lhotse doesn't support files with more than one FFMPEG source stream yet "
+                "(not to be confused with multi-channel)."
+            )
+            streamer.add_basic_audio_stream(frames_per_chunk=chunk_size)
+
+            begin_time = 0
+            end_time = self.chunk_size
+            for (chunk,) in streamer.stream():
+                yield {
+                    "recording_id": r.id,
+                    "begin_time": torch.as_tensor(begin_time, dtype=torch.float32),
+                    "end_time": torch.as_tensor(end_time, dtype=torch.float32),
+                    "audio": chunk.squeeze(),
+                }
+                begin_time += self.chunk_shift
+                end_time += self.chunk_shift
+
+
+def audio_chunk_collate(batch: List[Dict]):
+    audios = [d.pop("audio") for d in batch]
+    out = default_collate(batch)
+
+    maxlen = max(a.shape[0] for a in audios)
+    audio = torch.zeros((len(audios), maxlen))
+    for i, a in enumerate(audios):
+        audio[i, : a.shape[0]] = torch.as_tensor(a)
+    out["audio"] = audio
+
+    return out
+
+
+def audio_chunk_worker_init_fn(worker_id):
+    worker_info = torch.utils.data.get_worker_info()
+    dataset = worker_info.dataset  # the dataset copy in this worker process
+    overall_start = dataset.start
+    overall_end = dataset.end
+    # configure the dataset to only process the split workload
+    per_worker = int(
+        math.ceil((overall_end - overall_start) / float(worker_info.num_workers))
+    )
+    worker_id = worker_info.id
+    dataset.start = overall_start + worker_id * per_worker
+    dataset.end = min(dataset.start + per_worker, overall_end)
