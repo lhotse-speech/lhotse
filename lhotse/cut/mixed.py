cdb4dee2867da4a0d665f997978d84b539f41203
Yifan Yang <yifanyeung@qq.com>
2023-03-20 17:25:29 +0800

    Fix typo

--- lhotse/cut/mixed.py
+++ lhotse/cut/mixed.py
@@ -73,7 +73,7 @@ class MixedCut(Cut):
     Its primary purpose is to allow time-domain and feature-domain augmentation via mixing the training cuts
     with noise, music, and babble cuts. The actual mixing operations are performed on-the-fly.
 
-    Internally, :class:`~lhotse.cut.MixedCut` holds other cuts in multiple trakcs (:class:`~lhotse.cut.MixTrack`),
+    Internally, :class:`~lhotse.cut.MixedCut` holds other cuts in multiple tracks (:class:`~lhotse.cut.MixTrack`),
     each with its own offset and SNR that is relative to the first track.
 
     Please refer to the documentation of :class:`~lhotse.cut.Cut` to learn more about using cuts.
