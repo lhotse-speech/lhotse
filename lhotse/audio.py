d715cf7ae767431f343e9c53fb3593fb31b7ecec
Piotr Żelasko <petezor@gmail.com>
2023-05-12 13:06:26 -0400

    Update lhotse/audio.py

--- lhotse/audio.py
+++ lhotse/audio.py
@@ -191,7 +191,7 @@ class AudioSource:
                     "You requested a subset of a recording that is read from disk via a bash command. "
                     "Expect large I/O overhead if you are going to read many chunks like these, "
                     "since every time we will read the whole file rather than its subset."
-                    "You can use `caching.set_caching_enabled(True)` to mitigate the overhead."
+                    "You can use `lhotse.set_caching_enabled(True)` to mitigate the overhead."
                 )
 
             # Let's assume 'self.source' is a pipe-command with unchangeable file,
