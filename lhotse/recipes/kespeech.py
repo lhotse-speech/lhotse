b3815e98272c5bff02c2e38c7c3b773fb83e8213
zr_jin <60612200+JinZr@users.noreply.github.com>
2023-07-18 17:08:37 +0800

    added support for kespeech

--- lhotse/recipes/kespeech.py
+++ lhotse/recipes/kespeech.py
@@ -25,7 +25,12 @@ from lhotse.serialization import load_jsonl
 from lhotse.supervision import SupervisionSegment, SupervisionSet
 from lhotse.utils import Pathlike, compute_num_samples
 
-KE_SPEECH_PARTS = ("train", "dev", "test")
+KE_SPEECH_PARTS = ("train_phase1", "train_phase2", "dev_phase1", "dev_phase2", "test")
+
+
+def text_normalize(line: str) -> str:
+    line = line.replace("<SPOKEN_NOISE>", "")
+    return line
 
 
 def prepare_kespeech(
@@ -34,4 +39,92 @@ def prepare_kespeech(
     dataset_parts: Union[str, Sequence[str]] = "auto",
     num_jobs: int = 1,
 ):
-    pass
+    corpus_dir = Path(corpus_dir)
+    tasks_dir = corpus_dir / "Tasks" / "ASR"
+
+    assert corpus_dir.is_dir(), f"No such directory: {corpus_dir}"
+    assert tasks_dir.is_dir(), f"No such directory: {tasks_dir}"
+
+    manifests = defaultdict(dict)
+
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+    for part in KE_SPEECH_PARTS:
+        recordings = []
+        supervisions = []
+        with open(file=tasks_dir / part / "wav.scp") as wav_scp:
+            for line in wav_scp:
+                wav_id, wav_path = line.strip().split(maxsplit=1)
+                recordings.append(
+                    Recording(
+                        id=wav_id,
+                        sources=[
+                            AudioSource(
+                                type="file",
+                                channels=[0],
+                                source=str(corpus_dir / wav_path),
+                            )
+                        ],
+                        sampling_rate=16000,
+                        num_samples=compute_num_samples(corpus_dir / wav_path),
+                        duration=info(corpus_dir / wav_path).duration,
+                    )
+                )
+        with open(file=tasks_dir / part / "wav.scp") as wav_scp, open(
+            file=tasks_dir / part / "text"
+        ) as text, open(
+            file=tasks_dir / part / "utt2subdialect"
+        ) as utt2subdialect, open(
+            file=tasks_dir / part / "utt2spk"
+        ) as utt2spk:
+            for w_line, t_line, dialect_line, spk_line in zip(
+                wav_scp, text, utt2subdialect, utt2spk
+            ):
+                wav_id, wav_path = line.strip().split(maxsplit=1)
+                t_wav_id, transcript = t_line.strip().split(maxsplit=1)
+                d_wav_id, dialect = dialect_line.strip().split(maxsplit=1)
+                s_wav_id, speaker = spk_line.strip().split(maxsplit=1)
+                assert (
+                    wav_id == t_wav_id and t_wav_id == d_wav_id and d_wav_id == s_wav_id
+                )
+                recording = Recording(
+                    id=wav_id,
+                    sources=[
+                        AudioSource(
+                            type="file",
+                            channels=[0],
+                            source=str(corpus_dir / wav_path),
+                        )
+                    ],
+                    sampling_rate=16000,
+                    num_samples=compute_num_samples(corpus_dir / wav_path),
+                    duration=info(corpus_dir / wav_path).duration,
+                )
+                recordings.append(recording)
+                supervisions.append(
+                    SupervisionSegment(
+                        id=wav_id,
+                        recording_id=wav_id,
+                        start=0.0,
+                        duration=recording.duration,
+                        text=text_normalize(transcript.strip()),
+                        language=dialect,
+                        speaker=speaker,
+                    )
+                )
+        recording_set = RecordingSet.from_recordings(recordings)
+        supervision_set = SupervisionSet.from_segments(supervisions)
+        validate_recordings_and_supervisions(recording_set, supervision_set)
+
+        if output_dir is not None:
+            supervision_set.to_file(
+                output_dir / f"kespeech-asr_supervisions_{part}.jsonl.gz"
+            )
+            recording_set.to_file(
+                output_dir / f"kespeech-asr_recordings_{part}.jsonl.gz"
+            )
+
+        manifests[part] = {"recordings": recording_set, "supervisions": supervision_set}
+    return manifests
