55364d827ddf4946a7ce72ced969b8c420246a05
zr_jin <60612200+JinZr@users.noreply.github.com>
2023-07-19 09:55:57 +0800

    added CLI for kespeech

--- lhotse/bin/modes/recipes/__init__.py
+++ lhotse/bin/modes/recipes/__init__.py
@@ -38,6 +38,7 @@ from .heroico import *
 from .hifitts import *
 from .himia import *
 from .icsi import *
+from .kespeech import *
 from .l2_arctic import *
 from .libricss import *
 from .librilight import *
