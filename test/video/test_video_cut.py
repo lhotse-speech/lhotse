a266bfceefd5aafa2c2e3bc8dff7cc657c683b6c
Piotr Żelasko <petezor@gmail.com>
2023-09-14 22:00:56 -0400

    Stricter tests for padding and appending video cuts

--- test/video/test_video_cut.py
+++ test/video/test_video_cut.py
@@ -58,6 +58,9 @@ def test_video_mono_cut(video_cut):
 
 
 def test_video_mixed_cut_from_padding(video_cut):
+
+    orig_video, orig_audio = video_cut.load_video()
+
     video_cut = video_cut.pad(duration=10.0)
     assert video_cut.has_video
     assert video_cut.video.fps == FPS
@@ -72,8 +75,16 @@ def test_video_mixed_cut_from_padding(video_cut):
     assert audio.dtype == torch.float32
     assert audio.shape == (AUDIO_CHANNELS, 48000 * 10)
 
+    torch.testing.assert_close(video[:132], orig_video)
+    assert (video[132:] == 0).all()
+    torch.testing.assert_close(audio[:, :253440], orig_audio)
+    assert (audio[:, 253440:] == 0).all()
+
 
 def test_video_mixed_cut_from_appending(video_cut):
+
+    orig_video, orig_audio = video_cut.load_video()
+
     video_cut = video_cut.append(video_cut)
     assert video_cut.has_video
     assert video_cut.video.fps == FPS
@@ -87,3 +98,8 @@ def test_video_mixed_cut_from_appending(video_cut):
     assert video.shape == (132 * 2, COLOR, HEIGHT, WIDTH)
     assert audio.dtype == torch.float32
     assert audio.shape == (AUDIO_CHANNELS, 253440 * 2)
+
+    torch.testing.assert_close(video[:132], orig_video)
+    torch.testing.assert_close(video[132:], orig_video)
+    torch.testing.assert_close(audio[:, :253440], orig_audio)
+    torch.testing.assert_close(audio[:, 253440:], orig_audio)
