82bb80ae14752a3b73bfd72873757248b859c109
Piotr Żelasko <petezor@gmail.com>
2023-09-21 11:07:59 -0400

    Skip video test for PyTorch < 2.0

--- test/video/conftest.py
+++ test/video/conftest.py
@@ -3,6 +3,12 @@ from pathlib import Path
 import pytest
 
 from lhotse import Recording
+from lhotse.audio.backend import torchaudio_2_0_ffmpeg_enabled
+
+# Disable video tests for PyTorch/Torchaudio < 2.0
+collect_ignore = []
+if not torchaudio_2_0_ffmpeg_enabled():
+    collect_ignore_glob = ["test_video_*.py"]
 
 
 @pytest.fixture(scope="session")
