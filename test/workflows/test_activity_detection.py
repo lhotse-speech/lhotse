819e0202ea5f9c21992849c0c28717b9faaa5d0c
Vladislav A. Proskurov <rilshok@pm.me>
2023-09-25 13:43:38 +0400

    add test for silero vad in parallel

--- test/workflows/test_activity_detection.py
+++ test/workflows/test_activity_detection.py
@@ -1,5 +1,9 @@
-from lhotse import CutSet, SupervisionSegment
-from lhotse.workflows.activity_detection import SileroVAD16k
+from lhotse import CutSet, RecordingSet, SupervisionSegment
+from lhotse.workflows.activity_detection import (
+    ActivityDetectionProcessor,
+    SileroVAD8k,
+    SileroVAD16k,
+)
 
 
 def test_silero_vad_init():
@@ -12,3 +16,19 @@ def test_silero_vad_init():
     assert activity[0].start != 0
     assert activity[0].duration > 0.3
     assert activity[0].start + activity[0].duration < recording.duration
+
+
+def test_silero_vad_in_parallel():
+    cuts = CutSet.from_file("test/fixtures/ljspeech/cuts.json")
+    recordings = RecordingSet.from_recordings([cut.recording for cut in cuts])
+    processor = ActivityDetectionProcessor(SileroVAD8k, num_jobs=2, device="cpu")
+    supervisions = processor(recordings)
+    newcuts = CutSet.from_manifests(
+        recordings=recordings,
+        supervisions=supervisions,
+    )
+    assert len(newcuts) > 0
+    for cut in newcuts:
+        assert len(cut.supervisions) > 0
+        for sup in cut.supervisions:
+            assert sup.duration > 0
