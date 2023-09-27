4ed0669ef944c3072956e1d71b0213591e5af178
Vladislav A. Proskurov <rilshok@pm.me>
2023-09-27 17:45:39 +0400

    analyze audio and select speech fragments

--- lhotse/workflows/activity_detection/speach_only.py
+++ lhotse/workflows/activity_detection/speach_only.py
@@ -1,20 +1,46 @@
 from functools import partial
+from typing import Callable, List
 
 from lhotse.audio import Recording
 from lhotse.cut import CutSet
 
+from .base import Activity, ActivityDetector
+from .silero_vad import SileroVAD16k
+
 
 def convert_recording_to_mono(recording: Recording, sampling_rate: int) -> Recording:
     """Converts a recording to mono and resamples it to the given sampling rate"""
-    mono = recording  # TODO: Convert the recording to mono
+    mono = recording  # FIXME: Convert the recording to mono
     resampled = mono.resample(sampling_rate)
     return resampled
 
 
-def speach_only(cutset: CutSet, root: str, num_jobs: int = 1) -> CutSet:
+def make_activity_detector(device: str) -> Callable[[Recording], List[Activity]]:
+    detector = SileroVAD16k(device=device)
+    prepare = partial(convert_recording_to_mono, sampling_rate=detector.sampling_rate)
+    # TODO: Need to normalise the sound before analysis?
+
+    def get_detector() -> ActivityDetector:
+        return detector  # TODO: Get detector from current scope
+
+    def detect_activity(recording: Recording) -> List[Activity]:
+        """Detects activity timestamps in a recording"""
+        detector = get_detector()
+        track = prepare(recording).load_audio()
+        return detector.forward(track)
+
+    return detect_activity
+
+
+def speach_only(
+    cutset: CutSet,
+    root: str,
+    *,
+    device: str = "cpu",
+    num_jobs: int = 1,
+) -> CutSet:
     # TODO: 1. Act on cutset elements, for each cut:
-    mono_for_vad = partial(convert_recording_to_mono, sampling_rate=16000)
-    # TODO: 1.2 Analyze audio and select speech fragments
+    detect_activity = make_activity_detector(device=device)
     # TODO: 1.3 Transform audio by removing silence according to selected fragments
     # TODO: 1.4 Save new audio to root
     # TODO: 1.4 Redefine Recording
@@ -34,4 +60,4 @@ def speach_only(cutset: CutSet, root: str, num_jobs: int = 1) -> CutSet:
     # TODO: * Drop supervisions that are not part of the new audio
     # TODO: * Keep the additional supervision information (e.g., speaker, language, etc.)
     # TODO: * Use tqdm to show progress
-    return cutset
+    raise NotImplementedError()
