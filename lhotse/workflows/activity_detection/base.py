5a9f5f6bc34f952ef6662584c70e6c0de32522e3
Vladislav A. Proskurov <rilshok@pm.me>
2023-09-29 18:34:41 +0400

    add an iterable multiprocessor executor

--- lhotse/workflows/activity_detection/base.py
+++ lhotse/workflows/activity_detection/base.py
@@ -1,16 +1,37 @@
 import abc
 import multiprocessing
+import sys
+import warnings
 from concurrent.futures import ProcessPoolExecutor
+from contextlib import contextmanager
 from dataclasses import dataclass
 from functools import partial
 from itertools import chain
-from typing import Dict, List, Optional, Type
+from multiprocessing.context import BaseContext
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Generic,
+    Iterable,
+    List,
+    Optional,
+    Sequence,
+    Type,
+    TypeVar,
+    Union,
+)
 
 import numpy as np
 
 from lhotse.audio.recording import Recording
 from lhotse.audio.recording_set import RecordingSet
 from lhotse.supervision import SupervisionSegment, SupervisionSet
+
+if sys.version_info >= (3, 8):
+    from typing import Protocol
+else:
+    Protocol = object
 
 
 @dataclass
@@ -132,3 +153,109 @@ class ActivityDetectionProcessor:
                 ) from exc
             finally:
                 self._detectors.clear()
+
+
+ModelT_co = TypeVar("ModelT_co", covariant=True)
+ModelT_contra = TypeVar("ModelT_contra", contravariant=True)
+PredictT_co = TypeVar("PredictT_co", covariant=True)
+CaseT_contra = TypeVar("CaseT_contra", contravariant=True)
+
+
+class DoWork(Protocol[CaseT_contra, ModelT_contra, PredictT_co]):
+    def __call__(
+        self,
+        object: CaseT_contra,
+        model: ModelT_contra,
+        **kwargs: Any,
+    ) -> PredictT_co:
+        pass
+
+
+class ProcessWorker(Generic[CaseT_contra, ModelT_co, PredictT_co]):
+    models: Dict[Optional[int], ModelT_co] = {}
+
+    def __init__(
+        self,
+        gen_model: Callable[[], ModelT_co],
+        do_work: DoWork[CaseT_contra, ModelT_co, PredictT_co],
+        # "error", "ignore", "always", "default", "module", "once"
+        warnings_mode: Optional[str] = None,
+    ):
+        self._gen_model = gen_model
+        self._do_work = do_work
+        self._warnings_mode = warnings_mode
+
+    def _get_model(self) -> ModelT_co:
+        pid = multiprocessing.current_process().pid
+        model = self.models.get(pid)
+        if model is None:
+            model = self._gen_model()
+            self.models[pid] = model
+        return model
+
+    def __call__(self, obj: CaseT_contra) -> PredictT_co:
+        if self._warnings_mode is not None:
+            warnings.simplefilter(self._warnings_mode)
+        return self._do_work(obj, model=self._get_model())
+
+    def clear(self):
+        self.models.clear()
+
+
+class Processor(Generic[CaseT_contra, ModelT_co, PredictT_co]):
+    def __init__(
+        self,
+        worker: ProcessWorker[CaseT_contra, ModelT_co, PredictT_co],
+        *,
+        num_jobs: int,
+        verbose: bool = False,
+        mp_context: Optional[Union[BaseContext, str]] = None,
+    ):
+        self._worker = worker
+        self._num_jobs = num_jobs
+        self._verbose = verbose
+
+        if mp_context is None:
+            self._mp_context = None
+        else:
+            if isinstance(mp_context, str):
+                mp_context = multiprocessing.get_context(mp_context)
+            self._mp_context = mp_context
+
+    @contextmanager
+    def handle_errors(self, pool: ProcessPoolExecutor):
+        with pool as executor:
+            try:
+                yield executor
+            except KeyboardInterrupt as exc:  # pragma: no cover
+                pool.shutdown(wait=False)
+                if self._verbose:
+                    print("Processing interrupted by the user.")
+                raise exc
+            except Exception as exc:  # pragma: no cover
+                pool.shutdown(wait=False)
+                raise RuntimeError("Processing failed.") from exc
+            finally:
+                self._worker.clear()
+
+    def __call__(self, sequence: Sequence[CaseT_contra]) -> Iterable[PredictT_co]:
+        pool = ProcessPoolExecutor(
+            max_workers=self._num_jobs,
+            # initializer=...,
+            mp_context=self._mp_context,
+        )
+
+        with self.handle_errors(pool) as executor:
+            results = executor.map(self._worker, sequence)
+
+            if self._verbose:
+                from tqdm.auto import tqdm  # pylint: disable=import-outside-toplevel
+
+                results = tqdm(
+                    results,
+                    total=len(sequence),
+                    desc="Multiprocessing",
+                    unit="task",
+                )
+
+            yield from results
