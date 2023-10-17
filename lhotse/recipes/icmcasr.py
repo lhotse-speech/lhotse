a4701868e04a2156626cd4c087300de7b5ac689c
Yuekai Zhang <zhangyuekai@foxmail.com>
2023-10-17 22:42:36 +0800

    Support far field data for icmcasr challenge (#1189)

--- lhotse/recipes/icmcasr.py
+++ lhotse/recipes/icmcasr.py
@@ -15,68 +15,84 @@ from tqdm.auto import tqdm
 
 from lhotse.audio import Recording, RecordingSet
 from lhotse.qa import fix_manifests, validate_recordings_and_supervisions
-from lhotse.recipes.utils import manifests_exist
+from lhotse.recipes.utils import manifests_exist, normalize_text_alimeeting
 from lhotse.supervision import SupervisionSegment, SupervisionSet
-from lhotse.utils import Pathlike
+from lhotse.utils import Pathlike, is_module_available
 
-ICMCASR = ("train",)  # TODO: Support all subsets when released
+ICMCASR = ("train", "dev")  # TODO: Support all subsets when released
 POSITION = ("DA01", "DA02", "DA03", "DA04")
+SDM_POSITION = ("DX01C01", "DX02C01", "DX03C01", "DX04C01", "DX05C01", "DX06C01")
 
 
 def _parse_utterance(
     corpus_dir: Pathlike,
     section_path: Pathlike,
+    mic: str,
 ) -> Optional[Tuple[Recording, SupervisionSegment]]:
+    if not is_module_available("textgrid"):
+        raise ValueError(
+            "To prepare ICMC ASR data, please 'pip install textgrid' first."
+        )
+    import textgrid
+
     recordings = []
     segments = []
     for position in POSITION:
         text_path = (section_path / (position + ".TextGrid")).resolve()
         if not text_path.is_file():
             continue
+        if mic == "ihm":
+            audio_paths = [(section_path / (position + ".wav")).resolve()]
+            recording_ids = [
+                str(section_path / position)
+                .replace(str(corpus_dir) + "/", "")
+                .replace("/", "-")
+            ]
+        elif mic == "sdm":
+            audio_paths = [
+                (section_path / (sdm_position + ".wav")).resolve()
+                for sdm_position in SDM_POSITION
+            ]
+            recording_ids = [
+                str(section_path / sdm_position)
+                .replace(str(corpus_dir) + "/", "")
+                .replace("/", "-")
+                + f"-{position}"
+                for sdm_position in SDM_POSITION
+            ]
+        else:
+            raise ValueError(f"Unsupported mic type: {mic}")
 
-        audio_path = (section_path / (position + ".wav")).resolve()
-        recording_id = (
-            str(section_path / position)
-            .replace(str(corpus_dir) + "/", "")
-            .replace("/", "-")
-        )
+        for audio_path, recording_id in zip(audio_paths, recording_ids):
+            # check if audio_path exists, if not, then skip
+            if not audio_path.is_file():
+                # give some warning
+                logging.warning(f"Audio file {audio_path} does not exist - skipping.")
+                continue
+            recordings.append(
+                Recording.from_file(path=audio_path, recording_id=recording_id)
+            )
 
-        recordings.append(
-            Recording.from_file(path=audio_path, recording_id=recording_id)
-        )
-
-        with open(text_path) as f:
-            datalines = f.read().splitlines()
-
-        seq = 0
-        for dataline in datalines:
-            if "name" in dataline:
-                speaker = dataline.split('"')[1].strip()
-            elif "xmin =" in dataline:
-                start = float(dataline.split("=")[1].strip())
-            elif "xmax =" in dataline:
-                end = float(dataline.split("=")[1].strip())
-            elif "text" in dataline:
-                text = dataline.split('"')[1].strip()
-                if len(text) > 0:
-                    if float(recordings[-1].duration) < end:
-                        duration = float(recordings[-1].duration) - start
-                    else:
-                        duration = end - start
-                    segment_id = recording_id + "-" + str(seq)
-                    segments.append(
-                        SupervisionSegment(
-                            id=segment_id,
-                            recording_id=recording_id,
-                            start=start,
-                            duration=duration,
-                            channel=0,
-                            language="Chinese",
-                            speaker=speaker,
-                            text=text,
-                        )
+            tg = textgrid.TextGrid.fromFile(str(text_path))
+            assert len(tg.tiers) == 1, f"Expected 1 tier, found {len(tg.tiers)} tiers."
+            tier = tg.tiers[0]
+            speaker = tier.name
+            for i, interval in enumerate(tier.intervals):
+                if interval.mark != "":
+                    start = interval.minTime
+                    end = interval.maxTime
+                    text = interval.mark
+                    segment = SupervisionSegment(
+                        id=f"{recording_id}-{i}",
+                        recording_id=recording_id,
+                        start=start,
+                        duration=round(end - start, 4),
+                        channel=0,
+                        language="Chinese",
+                        speaker=speaker,
+                        text=normalize_text_alimeeting(text),
                     )
-                    seq += 1
+                    segments.append(segment)
 
     return recordings, segments
 
@@ -84,6 +100,7 @@ def _parse_utterance(
 def _prepare_subset(
     subset: str,
     corpus_dir: Pathlike,
+    mic: str,
     num_jobs: int = 1,
 ) -> Tuple[RecordingSet, SupervisionSet]:
     """
@@ -102,7 +119,7 @@ def _prepare_subset(
         supervision_set = []
         for section in tqdm(sections, desc="Distributing tasks"):
             section_path = part_path / section
-            futures.append(ex.submit(_parse_utterance, corpus_dir, section_path))
+            futures.append(ex.submit(_parse_utterance, corpus_dir, section_path, mic))
 
         for future in tqdm(futures, desc="Processing"):
             result = future.result()
@@ -125,6 +142,7 @@ def _prepare_subset(
 def prepare_icmcasr(
     corpus_dir: Pathlike,
     output_dir: Optional[Pathlike] = None,
+    mic: str = "ihm",
     num_jobs: int = 1,
 ) -> Dict[str, Dict[str, Union[RecordingSet, SupervisionSet]]]:
     """
@@ -152,19 +170,23 @@ def prepare_icmcasr(
         if manifests_exist(
             part=part,
             output_dir=output_dir,
-            prefix="icmcasr",
+            prefix=f"icmcasr-{mic}",
             suffix="jsonl.gz",
         ):
             logging.info(f"ICMC-ASR subset: {part} already prepared - skipping.")
             continue
 
-        recording_set, supervision_set = _prepare_subset(part, corpus_dir, num_jobs)
+        recording_set, supervision_set = _prepare_subset(
+            part, corpus_dir, mic, num_jobs
+        )
 
         if output_dir is not None:
             supervision_set.to_file(
-                output_dir / f"icmcasr_supervisions_{part}.jsonl.gz"
+                output_dir / f"icmcasr-{mic}_supervisions_{part}.jsonl.gz"
             )
-            recording_set.to_file(output_dir / f"icmcasr_recordings_{part}.jsonl.gz")
+            recording_set.to_file(
+                output_dir / f"icmcasr-{mic}_recordings_{part}.jsonl.gz"
+            )
 
         manifests[part] = {"recordings": recording_set, "supervisions": supervision_set}
 
