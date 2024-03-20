fb00a2ddffee09af116cf5aed357214710fd1db2
AmirHussein96 <amirnasser96@gmail.com>
2024-03-20 09:12:53 -0400

    .

--- lhotse/recipes/fisher_spanish_st.py
+++ lhotse/recipes/fisher_spanish_st.py
@@ -24,7 +24,6 @@ from pathlib import Path
 from typing import Dict, List, Optional, Tuple, Union
 
 from bs4 import BeautifulSoup
-
 from tqdm.auto import tqdm
 
 from lhotse.audio import RecordingSet
