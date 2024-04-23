51438a285d99aee32ac5792087d69d4c8c7a7779
Daniel Doña <daniel.dona@xnor.ga>
2024-04-23 15:32:11 +0200

    Undo

--- lhotse/serialization.py
+++ lhotse/serialization.py
@@ -123,13 +123,7 @@ def save_to_jsonl(data: Iterable[Dict[str, Any]], path: Pathlike) -> None:
     """Save the data to a JSON file. Will use GZip to compress it if the path ends with a ``.gz`` extension."""
     with open_best(path, "w") as f:
         for item in data:
-            try:
-                print(json.dumps(item, ensure_ascii=False), file=f)
-            except Exception as e:
-                print(e)
-                print(item)
-                
-                raise Exception("Serialization error!")
+            print(json.dumps(item, ensure_ascii=False), file=f)
 
 
 def load_jsonl(path: Pathlike) -> Generator[Dict[str, Any], None, None]:
