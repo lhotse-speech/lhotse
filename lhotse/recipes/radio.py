469ee97e4b4ee1fa55a44e4762a6af467c2cab7f
Matthew Wiesner <wiesner@jhu.edu>
2024-10-09 23:59:43 -0400

    Makes some small formatting changes

--- lhotse/recipes/radio.py
+++ lhotse/recipes/radio.py
@@ -35,36 +35,31 @@ and please cite as
 }
 """
 import json
-import logging
 from pathlib import Path
-from typing import Dict, Optional, Union, NamedTuple, Tuple, List
+from typing import Dict, Optional, Union
 from tqdm import tqdm
 from lhotse.audio import Recording, RecordingSet
 from lhotse.supervision import SupervisionSegment, SupervisionSet
 from lhotse.utils import Pathlike
-import lhotse
 import re
 
 from functools import partial
 
 from lhotse.parallel import parallel_map
 from lhotse.audio import set_ffmpeg_torchaudio_info_enabled
-import torchaudio
-import torch
 
 
-torchaudio.set_audio_backend("soundfile")
 set_ffmpeg_torchaudio_info_enabled(False)
 
 
 def _make_reco_and_sups_from_file(sf: str, msd: float = 0.5):
     corpus_dir = sf.parents[2]
-    audio_dir = corpus_dir / 'recos'
-    fname = sf.with_suffix('.flac').stem
+    audio_dir = corpus_dir / "recos"
+    fname = sf.with_suffix(".flac").stem
     
     # E.g. 2023_10_01_09h_02m_54s_dur30_ZnpbY9Zx_lat3.17_long113.04
-    chunk_idx = int(sf.parent.suffix.strip('.'))
-    reco_file = audio_dir / f'recos.{chunk_idx}' / f'{fname}.flac'
+    chunk_idx = int(sf.parent.suffix.strip("."))
+    reco_file = audio_dir / f"recos.{chunk_idx}" / f"{fname}.flac"
     reco = Recording.from_file(reco_file, recording_id=fname)
     reco.channel_ids = [0]
     sups = []
@@ -74,29 +69,29 @@ def _make_reco_and_sups_from_file(sf: str, msd: float = 0.5):
     
     # Parse the file format, shown in the comment above, to get:
     # date, station, latitude, longitude, and the estimated gender 
-    lat, lon = re.search(r'lat[^_]+_long[^_]+', Path(sf).stem).group(0).split("_")
-    lat = float(lat.replace('lat', ''))
-    lon = float(lon.replace('long', ''))
-    station = re.search(r's_dur[0-9]+_(.*)_lat[^_]+_long[^_]+', fname).groups()[0] #.split('_')
-    fname_vals = fname.split('_')
-    date = [int(i.strip('hms')) for i in fname_vals[0:6]] # YY MM DD hh mm ss
+    lat, lon = re.search(r"lat[^_]+_long[^_]+", Path(sf).stem).group(0).split("_")
+    lat = float(lat.replace("lat", ""))
+    lon = float(lon.replace("long", ""))
+    station = re.search(r"s_dur[0-9]+_(.*)_lat[^_]+_long[^_]+", fname).groups()[0]
+    fname_vals = fname.split("_")
+    date = [int(i.strip("hms")) for i in fname_vals[0:6]] # YY MM DD hh mm ss
     for seg in segments:
         start, end = float(seg[1]), float(seg[2])
         dur = end - start
-        if seg[0] in ('male', 'female') and dur > msd:
+        if seg[0] in ("male", "female") and dur > msd:
             sups.append(
                 SupervisionSegment(
-                    id=f'{fname}_{int(100*start):04}',
+                    id=f"{fname}_{int(100*start):04}",
                     recording_id=fname,
                     start=start,
                     duration=round(dur, 4),
                     channel=0,
                     custom={
-                        'date': date,
-                        'lat': lat,
-                        'lon': lon,
-                        'station': station,
-                        'est_gender': seg[0],
+                        "date": date,
+                        "lat": lat,
+                        "lon": lon,
+                        "station": station,
+                        "est_gender": seg[0],
                     },
                 )
             )
@@ -117,20 +112,24 @@ def prepare_radio(
         keys 'recordings' and 'supervisions'.  
     """
     corpus_dir = Path(corpus_dir)
-    segment_files = corpus_dir.rglob('segs/*/*.json')
+    segment_files = corpus_dir.rglob("segs/*/*.json")
     supervisions, recordings = [], []
     fun = partial(_make_reco_and_sups_from_file, msd=min_segment_duration)
     output_dir = Path(output_dir) if output_dir is not None else None
     output_dir.mkdir(mode=511, parents=True, exist_ok=True)
-    with RecordingSet.open_writer(output_dir / "radio_recordings.jsonl.gz") as rec_writer:
-        with SupervisionSet.open_writer(output_dir / "radio_supervisions.jsonl.gz") as sup_writer:
+    with RecordingSet.open_writer(
+        output_dir / "radio_recordings.jsonl.gz"
+    ) as rec_writer:
+        with SupervisionSet.open_writer(
+            output_dir / "radio_supervisions.jsonl.gz"
+        ) as sup_writer:
             for sups, reco in tqdm(
                 parallel_map(
                     fun,
                     segment_files,
                     num_jobs=num_jobs,
                 ),
-                desc=f'Making recordings and supervisions',
+                desc=f"Making recordings and supervisions",
             ):
                 rec_writer.write(reco)
                 for sup in sups:
