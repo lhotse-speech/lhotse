c49a75023d89b3da33c236a672d3f392b3764b52
Roman Korostik <rkorostik@nvidia.com>
2025-02-11 19:18:01 +0400

    lowpass augmentation: scipy import guard

--- lhotse/augmentation/lowpass.py
+++ lhotse/augmentation/lowpass.py
@@ -2,10 +2,10 @@ from dataclasses import dataclass
 from typing import Literal
 
 import numpy as np
-import scipy.signal
 import soundfile as sf
 
 from lhotse.augmentation.transform import AudioTransform
+from lhotse.utils import is_module_available
 
 
 @dataclass
@@ -23,6 +23,13 @@ class Lowpass(AudioTransform):
         samples: np.ndarray,
         sampling_rate: int,
     ):
+        if not is_module_available("scipy"):
+            raise ImportError(
+                "In order to use Lowpass transforms, run 'pip install scipy'"
+            )
+
+        import scipy.signal
+
         N, _ = scipy.signal.kaiserord(ripple=100, width=0.05)
         taps = scipy.signal.firwin(
             numtaps=N,
